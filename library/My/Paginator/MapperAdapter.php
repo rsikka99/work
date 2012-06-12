0f30e54ad83f50c8f3fb7d652e7bec11c3bc2748
Shawn Wilder <swilder@tangentmtw.com>
2012-06-12 16:37:07 -0400

    Added a paginator mapper adapter so that we can use pagination when dealing with models.

---
+++ library/My/Paginator/MapperAdapter.php
@@ -0,0 +1,54 @@
+<?php
+
+class My_Paginator_MapperAdapter implements Zend_Paginator_Adapter_Interface
+{
+    /**
+     * An instance of a mapper to use to map objects
+     *
+     * @var My_Model_Mapper_Abstract
+     */
+    protected $_mapper;
+    
+    /**
+     * The where array to use with the count and fetchall functions
+     * 
+     * @var unknown_type
+     */
+    protected $_where;
+    
+    /**
+     * An array of objects
+     *
+     * @var array<My_Model_Abstract>
+     */
+    protected $_objects = array ();
+
+    public function __construct (My_Model_Mapper_Abstract $mapper, $where = null)
+    {
+        $this->_mapper = $mapper;
+        $this->_where = $where;
+    }
+
+    /**
+     * Returns an array of items for a page.
+     *
+     * @param integer $offset
+     *            Page offset
+     * @param integer $itemCountPerPage
+     *            Number of items per page
+     * @return array
+     */
+    public function getItems ($offset, $itemCountPerPage)
+    {
+        $this->_objects = $this->_mapper->fetchAll($this->_where, null, $itemCountPerPage, $offset);
+        return $this->_objects;
+    }
+    
+    /*
+     * (non-PHPdoc) @see Countable::count()
+     */
+    public function count ()
+    {
+        return $this->_mapper->count($this->_where);
+    }
+}
