f2b7a358b8fa2b50f251f34ce2d198fd2498ea38
Lee Robert <lrobert@tangentmtw.com>
2012-06-15 10:04:18 -0400

    Added model and mapper for option

---
+++ application/modules/quotegen/models/mappers/Option.php
@@ -0,0 +1,207 @@
+<?php
+
+class Quotegen_Model_Mapper_Option extends My_Model_Mapper_Abstract
+{
+    /**
+     * The default db table class to use
+     *
+     * @var String
+     *
+     */
+    protected $_defaultDbTable = 'Quotegen_Model_DbTable_Option';
+
+    /**
+     * Gets an instance of the mapper
+     *
+     * @return Quotegen_Model_Mapper_Option
+     */
+    public static function getInstance ()
+    {
+        return self::getCachedInstance();
+    }
+
+    /**
+     * Saves an instance of Quotegen_Model_Option to the database.
+     * If the id is null then it will insert a new row
+     *
+     * @param $option Quotegen_Model_Option
+     *            The object to insert
+     * @return mixed The primary key of the new row
+     */
+    public function insert (&$object)
+    {
+        // Get an array of data to save
+        $data = $object->toArray();
+        
+        // Remove the id
+        unset($data ['id']);
+        
+        // Insert the data
+        $id = $this->getDbTable()->insert($data);
+        
+        $object->setId($id);
+        
+        // Save the object into the cache
+        $this->saveItemToCache($object, $id);
+        
+        return $id;
+    }
+
+    /**
+     * Saves (updates) an instance of Quotegen_Model_Option to the database.
+     *
+     * @param $option Quotegen_Model_Option
+     *            The option model to save to the database
+     * @param $primaryKey mixed
+     *            Optional: The original primary key, in case we're changing it
+     * @return int The number of rows affected
+     */
+    public function save ($object, $primaryKey = null)
+    {
+        $data = $this->unsetNullValues($object->toArray());
+        
+        if ($primaryKey === null)
+        {
+            $primaryKey = $data ['id'];
+        }
+        
+        // Update the row
+        $rowsAffected = $this->getDbTable()->update($data, array (
+                'id = ?' => $primaryKey 
+        ));
+        
+        // Save the object into the cache
+        $this->saveItemToCache($object, $primaryKey);
+        
+        return $rowsAffected;
+    }
+
+    /**
+     * Saves an instance of Quotegen_Model_Option to the database.
+     * If the id is null then it will insert a new row
+     *
+     * @param $option mixed
+     *            This can either be an instance of Quotegen_Model_Option or the
+     *            primary key to delete
+     * @return mixed The primary key of the new row
+     */
+    public function delete ($option)
+    {
+        if ($option instanceof Quotegen_Model_Option)
+        {
+            $whereClause = array (
+                    'id = ?' => $option->getId() 
+            );
+        }
+        else
+        {
+            $whereClause = array (
+                    'id = ?' => $option 
+            );
+        }
+        
+        $result = $this->getDbTable()->delete($whereClause);
+        return $result;
+    }
+
+    /**
+     * Finds a option based on it's primaryKey
+     *
+     * @param $id int
+     *            The id of the option to find
+     * @return void Quotegen_Model_Option
+     */
+    public function find ($id)
+    {
+        // Get the item from the cache and return it if we find it.
+        $result = $this->getItemFromCache($id);
+        if ($result instanceof Quotegen_Model_Option)
+        {
+            return $result;
+        }
+        
+        // Assuming we don't have a cached object, lets go get it.
+        $result = $this->getDbTable()->find($id);
+        if (0 == count($result))
+        {
+            return;
+        }
+        $row = $result->current();
+        $object = new Quotegen_Model_Option($row->toArray());
+        
+        // Save the object into the cache
+        $this->saveItemToCache($object, $id);
+        
+        return $object;
+    }
+
+    /**
+     * Fetches a option
+     *
+     * @param $where string|array|Zend_Db_Table_Select
+     *            OPTIONAL An SQL WHERE clause or Zend_Db_Table_Select object.
+     * @param $order string|array
+     *            OPTIONAL An SQL ORDER clause.
+     * @param $offset int
+     *            OPTIONAL An SQL OFFSET value.
+     * @return void Quotegen_Model_Option
+     */
+    public function fetch ($where = null, $order = null, $offset = null)
+    {
+        $row = $this->getDbTable()->fetchRow($where, $order, $offset);
+        if (is_null($row))
+        {
+            return;
+        }
+        
+        $object = new Quotegen_Model_Option($row->toArray());
+        
+        // Save the object into the cache
+        $this->saveItemToCache($object, $object->getId());
+        
+        return $object;
+    }
+
+    /**
+     * Fetches all options
+     *
+     * @param $where string|array|Zend_Db_Table_Select
+     *            OPTIONAL An SQL WHERE clause or Zend_Db_Table_Select object.
+     * @param $order string|array
+     *            OPTIONAL An SQL ORDER clause.
+     * @param $count int
+     *            OPTIONAL An SQL LIMIT count. (Defaults to 25)
+     * @param $offset int
+     *            OPTIONAL An SQL LIMIT offset.
+     * @return multitype:Quotegen_Model_Option
+     */
+    public function fetchAll ($where = null, $order = null, $count = 25, $offset = null)
+    {
+        $resultSet = $this->getDbTable()->fetchAll($where, $order, $count, $offset);
+        $entries = array ();
+        foreach ( $resultSet as $row )
+        {
+            $object = new Quotegen_Model_Option($row->toArray());
+            
+            // Save the object into the cache
+            $this->saveItemToCache($object, $object->getId());
+            
+            $entries [] = $object;
+        }
+        return $entries;
+    }
+
+    /**
+     * Gets a where clause for filtering by id
+     *
+     * @param unknown_type $id            
+     * @return array
+     */
+    public function getWhereId ($id)
+    {
+        return array (
+                'id = ?' => $id 
+        );
+    }
+}
+
