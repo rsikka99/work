72b67753688f131a4aecbf25d84c7efffdd10aaf
Lee Robert <lrobert@tangentmtw.com>
2012-06-15 15:16:35 -0400

    Moved paginator to _partials in the quotegen module. Also set the partial in the module bootstrap

--- application/modules/quotegen/Bootstrap.php
+++ application/modules/quotegen/Bootstrap.php
@@ -3,6 +3,9 @@
 class Quotegen_Bootstrap extends Zend_Application_Module_Bootstrap
 {
 
+    /**
+     * Adds our navigation menus to the main navigation
+     */
     protected function _initNavigation ()
     {
         $view = $this->getApplication()->getResource('view');
@@ -10,6 +13,14 @@ class Quotegen_Bootstrap extends Zend_Application_Module_Bootstrap
         /* @var $container Zend_Navigation */
         $container = $view->navigation()->getContainer();
         $container->addPages($config);
+    }
+
+    /**
+     * Sets the paginator view partials
+     */
+    protected function _initPaginatorViewPartial ()
+    {
+        Zend_View_Helper_PaginationControl::setDefaultViewPartial('_partials/paginator.phtml');
     }
 }
 
