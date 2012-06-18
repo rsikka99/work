4a71a052483fc922026b743dc0d3d8fe575644ad
Shawn Wilder <swilder@tangentmtw.com>
2012-06-18 08:56:25 -0400

    Changed quoteDevice mapper to reflect quoteDeivice objects instead of template

--- application/modules/quotegen/models/mappers/QuoteDevice.php
+++ application/modules/quotegen/models/mappers/QuoteDevice.php
@@ -1,6 +1,6 @@
 <?php
 
-class Quotegen_Model_Mapper_Template extends My_Model_Mapper_Abstract
+class Quotegen_Model_Mapper_QuoteDevice extends My_Model_Mapper_Abstract
 {
     /**
      * The default db table class to use
@@ -8,12 +8,12 @@ class Quotegen_Model_Mapper_Template extends My_Model_Mapper_Abstract
      * @var String
      *
      */
-    protected $_defaultDbTable = 'Quotegen_Model_DbTable_Template';
+    protected $_defaultDbTable = 'Quotegen_Model_DbTable_QuoteDevice';
 
     /**
      * Gets an instance of the mapper
      *
-     * @return Quotegen_Model_Mapper_Template
+     * @return Quotegen_Model_Mapper_QuoteDevice
      */
     public static function getInstance ()
     {
@@ -21,10 +21,10 @@ class Quotegen_Model_Mapper_Template extends My_Model_Mapper_Abstract
     }
 
     /**
-     * Saves an instance of Quotegen_Model_Template to the database.
+     * Saves an instance of Quotegen_Model_QuoteDevice to the database.
      * If the id is null then it will insert a new row
      *
-     * @param $object Quotegen_Model_Template
+     * @param $object Quotegen_Model_QuoteDevice
      *            The object to insert
      * @return mixed The primary key of the new row
      */
@@ -48,10 +48,10 @@ class Quotegen_Model_Mapper_Template extends My_Model_Mapper_Abstract
     }
 
     /**
-     * Saves (updates) an instance of Quotegen_Model_Template to the database.
+     * Saves (updates) an instance of Quotegen_Model_QuoteDevice to the database.
      *
-     * @param $object Quotegen_Model_Template
-     *            The template model to save to the database
+     * @param $object Quotegen_Model_QuoteDevice
+     *            The quoteDevice model to save to the database
      * @param $primaryKey mixed
      *            Optional: The original primary key, in case we're changing it
      * @return int The number of rows affected
@@ -80,13 +80,13 @@ class Quotegen_Model_Mapper_Template extends My_Model_Mapper_Abstract
      * Deletes rows from the database.
      *
      * @param $object mixed
-     *            This can either be an instance of Quotegen_Model_Template or the
+     *            This can either be an instance of Quotegen_Model_QuoteDevice or the
      *            primary key to delete
      * @return mixed The number of rows deleted
      */
     public function delete ($object)
     {
-        if ($object instanceof Quotegen_Model_Template)
+        if ($object instanceof Quotegen_Model_QuoteDevice)
         {
             $whereClause = array (
                     'id = ?' => $object->getId() 
@@ -104,17 +104,17 @@ class Quotegen_Model_Mapper_Template extends My_Model_Mapper_Abstract
     }
 
     /**
-     * Finds a template based on it's primaryKey
+     * Finds a quoteDevice based on it's primaryKey
      *
      * @param $id int
-     *            The id of the template to find
-     * @return void Quotegen_Model_Template
+     *            The id of the quoteDevice to find
+     * @return void Quotegen_Model_QuoteDevice
      */
     public function find ($id)
     {
         // Get the item from the cache and return it if we find it.
         $result = $this->getItemFromCache($id);
-        if ($result instanceof Quotegen_Model_Template)
+        if ($result instanceof Quotegen_Model_QuoteDevice)
         {
             return $result;
         }
@@ -126,7 +126,7 @@ class Quotegen_Model_Mapper_Template extends My_Model_Mapper_Abstract
             return;
         }
         $row = $result->current();
-        $object = new Quotegen_Model_Template($row->toArray());
+        $object = new Quotegen_Model_QuoteDevice($row->toArray());
         
         // Save the object into the cache
         $this->saveItemToCache($object, $id);
@@ -135,7 +135,7 @@ class Quotegen_Model_Mapper_Template extends My_Model_Mapper_Abstract
     }
 
     /**
-     * Fetches a template
+     * Fetches a quoteDevice
      *
      * @param $where string|array|Zend_Db_Table_Select
      *            OPTIONAL An SQL WHERE clause or Zend_Db_Table_Select object.
@@ -143,7 +143,7 @@ class Quotegen_Model_Mapper_Template extends My_Model_Mapper_Abstract
      *            OPTIONAL An SQL ORDER clause.
      * @param $offset int
      *            OPTIONAL An SQL OFFSET value.
-     * @return void Quotegen_Model_Template
+     * @return void Quotegen_Model_QuoteDevice
      */
     public function fetch ($where = null, $order = null, $offset = null)
     {
@@ -153,7 +153,7 @@ class Quotegen_Model_Mapper_Template extends My_Model_Mapper_Abstract
             return;
         }
         
-        $object = new Quotegen_Model_Template($row->toArray());
+        $object = new Quotegen_Model_QuoteDevice($row->toArray());
         
         // Save the object into the cache
         $this->saveItemToCache($object, $object->getId());
@@ -162,7 +162,7 @@ class Quotegen_Model_Mapper_Template extends My_Model_Mapper_Abstract
     }
 
     /**
-     * Fetches all templates
+     * Fetches all quoteDevices
      *
      * @param $where string|array|Zend_Db_Table_Select
      *            OPTIONAL An SQL WHERE clause or Zend_Db_Table_Select object.
@@ -172,7 +172,7 @@ class Quotegen_Model_Mapper_Template extends My_Model_Mapper_Abstract
      *            OPTIONAL An SQL LIMIT count. (Defaults to 25)
      * @param $offset int
      *            OPTIONAL An SQL LIMIT offset.
-     * @return multitype:Quotegen_Model_Template
+     * @return multitype:Quotegen_Model_QuoteDevice
      */
     public function fetchAll ($where = null, $order = null, $count = 25, $offset = null)
     {
@@ -180,7 +180,7 @@ class Quotegen_Model_Mapper_Template extends My_Model_Mapper_Abstract
         $entries = array ();
         foreach ( $resultSet as $row )
         {
-            $object = new Quotegen_Model_Template($row->toArray());
+            $object = new Quotegen_Model_QuoteDevice($row->toArray());
             
             // Save the object into the cache
             $this->saveItemToCache($object, $object->getId());
