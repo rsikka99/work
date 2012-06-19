99e4b0dbf3d04e62681c88e48ead68ebdcd66aed
John Sadler <jsadler@tangentmtw.com>
2012-06-19 09:15:03 -0400

    Added Term and Range forms

---
+++ application/modules/quotegen/forms/LeasingSchemaTerm.php
@@ -0,0 +1,65 @@
+<?php
+
+class Quotegen_Form_LeasingSchemaTerm extends EasyBib_Form
+{
+
+    public function __construct ($options = null, $type = null)
+    {
+        //call parent contsructor
+        parent::__construct($options);
+        $elements = array ();
+        $elementCounter = 0;
+        
+        /**
+         * Add class to form for label alignment
+         *
+         * - Vertical .form-vertical (not required)	Stacked, left-aligned labels
+         * over controls (default)
+         * - Inline .form-inline Left-aligned label and inline-block controls
+         * for compact style
+         * - Search .form-search Extra-rounded text input for a typical search
+         * aesthetic
+         * - Horizontal .form-horizontal
+         *
+         * Use .form-horizontal to have same experience as with Bootstrap v1!
+         */
+        $this->setAttrib('class', 'form-horizontal');
+        $this->setName('leasingSchemaTerm');
+        $this->setAttrib('id', 'leasingSchemaTerm');
+
+        // Set the method for the display form to POST
+        $this->setMethod('POST');
+        
+        $this->addElement('text', 'name', array (
+                'label' => 'Company Name:', 
+                'required' => true, 
+                'filters' => array (
+                        'StringTrim', 
+                        'StripTags' 
+                ), 
+                'validators' => array (
+                        array (
+                                'validator' => 'StringLength', 
+                                'options' => array (
+                                        1, 
+                                        255 
+                                ) 
+                        ) 
+                ) 
+        ));
+        
+        // Add the submit button
+        $this->addElement('submit', 'submit', array (
+                'ignore' => true, 
+                'label' => 'Save' 
+        ));
+        
+        // Add the cancel button
+        $this->addElement('submit', 'cancel', array (
+                'ignore' => true, 
+                'label' => 'Cancel' 
+        ));
+        
+        EasyBib_Form_Decorator::setFormDecorator($this, EasyBib_Form_Decorator::BOOTSTRAP, 'submit', 'cancel');
+    }
+}
