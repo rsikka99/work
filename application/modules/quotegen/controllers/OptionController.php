4129ecd56cdd68f7a1f430363a5ba4ffdfe5fe7e
Shawn Wilder <swilder@tangentmtw.com>
2012-06-21 10:47:42 -0400

    Created OptionController with crud operations, with form to use for edit / delete

--- application/modules/quotegen/controllers/OptionController.php
+++ application/modules/quotegen/controllers/OptionController.php
@@ -7,12 +7,11 @@ class Quotegen_OptionController extends Zend_Controller_Action
     public function init ()
     {
     }
-    
 
     public function indexAction ()
     {
         // Get all current items in categories table
-        $optionMapper = new Quotegen_Model_Mapper_Option();
+        $optionMapper = Quotegen_Model_Mapper_Option::getInstance();
         $paginator = new Zend_Paginator(new My_Paginator_MapperAdapter($optionMapper));
         
         // Set current page
@@ -27,22 +26,208 @@ class Quotegen_OptionController extends Zend_Controller_Action
 
     public function deleteAction ()
     {
-   
+        $optionId = $this->_getParam('id', false);
+        
+        if (! $optionId)
+        {
+            $this->_helper->flashMessenger(array (
+                    'warning' => 'Please select an option to delete first.' 
+            ));
+            $this->_helper->redirector('index');
+        }
+        
+        $optionMapper = Quotegen_Model_Mapper_Option::getInstance();
+        $option = $optionMapper->find($optionId);
+        
+        if (! $option)
+        {
+            $this->_helper->flashMessenger(array (
+                    'danger' => 'There was an error finding that option to delete.' 
+            ));
+            $this->_helper->redirector('index');
+        }
+        
+        $message = "Are you sure you want to delete {$option->getName()}?";
+        $form = new Application_Form_Delete($message);
+        
+        $request = $this->getRequest();
+        if ($request->isPost())
+        {
+            $values = $request->getPost();
+            if (! isset($values ['cancel']))
+            {
+                // delete quote from database
+                if ($form->isValid($values))
+                {
+                    Quotegen_Model_Mapper_OptionCategory::getInstance()->deleteByOptionId($option->getId());
+                    $optionMapper->delete($option);
+                    
+                    $this->_helper->flashMessenger(array (
+                            'success' => "Option  {$this->view->escape ( $option->getName() )} was deleted successfully." 
+                    ));
+                    $this->_helper->redirector('index');
+                }
+            }
+            else // go back
+            {
+                $this->_helper->redirector('index');
+            }
+        }
+        $this->view->form = $form;
     }
 
     public function createAction ()
     {
- 
+        $form = new Quotegen_Form_Option();
+        // If the form is on post insert data
+        $request = $this->getRequest();
+        
+        if ($request->isPost())
+        {
+            // Get values from the form
+            $values = $request->getPost();
+            if (! isset($values ['cancel']))
+            {
+                try
+                {
+                    if ($form->isValid($values))
+                    {
+                        // Persist data to database
+                        $categoryMapper = new Quotegen_Model_Mapper_Category();
+                        $category = new Quotegen_Model_Category();
+                        $category->populate($values);
+                        
+                        $categoryMapper->insert($category);
+                        
+                        // Redirect client back to index
+                        $this->_helper->redirector('index');
+                    }
+                    else // Values in form data aren't valid.
+                    {
+                        throw new InvalidArgumentException('Please correct the fields below');
+                    }
+                }
+                catch ( Exception $e )
+                {
+                    $this->_helper->flashMessenger(array (
+                            'danger' => $e->getMessage() 
+                    ));
+                }
+            }
+            else // Cancel was hit: redirect user
+            {
+                $this->_helper->redirector('index');
+            }
+        }
+        
+        $this->view->form = $form;
     }
 
     public function editAction ()
     {
-    
+        $optionId = $this->_getParam('id', false);
+        
+        // If not idea is set then back to index page
+        if (! $optionId)
+        {
+            $this->_helper->flashMessenger(array (
+                    'warning' => 'Please select an option first.' 
+            ));
+            // Redirect
+            $this->_helper->redirector('index');
+        }
+        
+        // Load the form for use
+        $form = new Quotegen_Form_Option();
+        
+        // Find the option by id
+        $optionMapper = Quotegen_Model_Mapper_Option::getInstance();
+        $option = $optionMapper->find($optionId);
+        $optionValues = $option->toArray();
+        /* @var $category Quotegen_Model_Category */
+        foreach ( $option->getCategories() as $category )
+        {
+            $optionValues ['categories'] [] = $category->getId();
+        }
+        
+        $form->populate($optionValues);
+        $request = $this->getRequest();
+        
+        // If request is on post update the record
+        if ($request->isPost())
+        {
+            
+            $values = $request->getPost();
+            if (! isset($values ['cancel']))
+            {
+                try
+                {
+                    // Validate the form
+                    if ($form->isValid($values))
+                    {
+                        // Update quotesetting and message to comfirm
+                        $option->populate($values);
+                        
+                        $optionCategoryMapper = Quotegen_Model_Mapper_OptionCategory::getInstance();
+                        // Create a new category since we know the option id will stay the same at all times.
+                        $optionCategory = new Quotegen_Model_OptionCategory();
+                        $optionCategory->setOptionId($option->getId());
+                        
+                        $categoryIds [] = array ();
+                        /* @var $category Quotegen_Model_Category */
+                        foreach ( $option->getCategories() as $category )
+                        {
+                            
+                            if (array_search((string)$category->getId(), $values ['categories']) === false)
+                            {
+                                $optionCategory->setCategoryId($category->getId());
+                                $optionCategoryMapper->delete($optionCategory);
+                            }
+                            else
+                            {
+                                $categoryIds [] = $category->getId();
+                            }
+                        }
+                        
+                        foreach ( $values ['categories'] as $categoryPostId )
+                        {
+                            if (array_search($categoryPostId, $categoryIds) === false)
+                            {
+                                $optionCategory->setCategoryId($categoryPostId);
+                                $optionCategoryMapper->insert($optionCategory);
+                            }
+                        }
+                        $optionMapper->save($option, $optionId);
+                        $this->_helper->flashMessenger(array (
+                                'success' => "Category setting was updated sucessfully." 
+                        ));
+                        
+                        $this->_helper->redirector('index');
+                    }
+                    else
+                    {
+                        throw new InvalidArgumentException("Please correct the errors below");
+                    }
+                }
+                catch ( InvalidArgumentException $e )
+                {
+                    $this->_helper->flashMessenger(array (
+                            'danger' => $e->getMessage() 
+                    ));
+                }
+            }
+            else // Client hit cancel redicect
+            {
+                // User has cancelled. We could do a redirect here if we wanted.
+                $this->_helper->redirector('index');
+            }
+        }
+        
+        $this->view->form = $form;
     }
 
     public function viewAction ()
     {
-
     }
 }
 
---
+++ application/modules/quotegen/forms/Option.php
@@ -0,0 +1,110 @@
+<?php
+
+class Quotegen_Form_Option extends EasyBib_Form
+{
+
+    public function init ()
+    {
+        // Set the method for the display form to POST
+        $this->setMethod('POST');
+        /**
+         * Add class to form for label alignment
+         *
+         * - Vertical .form-vertical (not required)	Stacked, left-aligned labels
+         * over controls (default)
+         * - Inline .form-inline Left-aligned label and inline-block controls
+         * for compact style
+         * - Search .form-search Extra-rounded text input for a typical search
+         * aesthetic
+         * - Horizontal .form-horizontal
+         *
+         * Use .form-horizontal to have same experience as with Bootstrap v1!
+         */
+        $this->setAttrib('class', 'form-horizontal');
+        
+        $this->addElement('text', 'name', array (
+                'label' => 'Name:', 
+                'required' => true, 
+                
+                'filters' => array (
+                        'StringTrim', 
+                        'StripTags' 
+                ), 
+                'validator' => 'StringLength', 
+                'options' => array (
+                        1, 
+                        255 
+                ) 
+        ));
+        
+        $this->addElement('textArea', 'description', array (
+                'label' => 'Description:', 
+                'required' => true, 
+                'style' => 'height: 100px', 
+                'filters' => array (
+                        'StringTrim', 
+                        'StripTags' 
+                ), 
+                'validator' => 'StringLength', 
+                'options' => array (
+                        1, 
+                        255 
+                ) 
+        ));
+        
+        $this->addElement('text', 'price', array (
+                'label' => 'Price:', 
+                'required' => true, 
+                'filters' => array (
+                        'StringTrim', 
+                        'StripTags' 
+                ), 
+                'validators' => array (
+                        'Float'
+                        )
+        ));
+        
+        $this->addElement('text', 'sku', array (
+                'label' => 'Sku:', 
+                'required' => true, 
+                
+                'filters' => array (
+                        'StringTrim', 
+                        'StripTags' 
+                ), 
+                'validator' => 'StringLength', 
+                'options' => array (
+                        1, 
+                        255 
+                ) 
+        ));
+        
+        $optionCategoryCheckBox = new Zend_Form_Element_MultiCheckbox('categories', array (
+                'label' => 'Options:' 
+        ));
+        
+        /* @var $category Quotegen_Model_Category */
+        foreach ( Quotegen_Model_Mapper_Category::getInstance()->fetchAll() as $category )
+        {
+            $optionCategoryCheckBox->addMultiOption($category->getId(), $category->getName());
+        }
+        
+        $this->addElement($optionCategoryCheckBox);
+        
+        // Add the submit button
+        $this->addElement('submit', 'submit', array (
+                'ignore' => true, 
+                'label' => 'Save' 
+        ));
+        
+        // Add the cancel button
+        $this->addElement('submit', 'cancel', array (
+                'ignore' => true, 
+                'label' => 'Cancel' 
+        ));
+        
+        EasyBib_Form_Decorator::setFormDecorator($this, EasyBib_Form_Decorator::BOOTSTRAP, 'submit', 'cancel');
+    }
+}
+
+?>
