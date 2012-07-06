8cdcaceb1d718cc345f27140d2ff0f74641e4cb1
Shawn Wilder <swilder@tangentmtw.com>
2012-07-06 13:40:23 -0400

    Started added functionality to save quote settings inside the quote build process

--- application/modules/quotegen/controllers/Quote/SettingsController.php
+++ application/modules/quotegen/controllers/Quote/SettingsController.php
@@ -2,6 +2,7 @@
 
 class Quotegen_Quote_SettingsController extends Quotegen_Library_Controller_Quote
 {
+
     public function init ()
     {
         parent::init();
@@ -13,6 +14,55 @@ class Quotegen_Quote_SettingsController extends Quotegen_Library_Controller_Quot
      */
     public function indexAction ()
     {
+        $quote = $this->_quote;
+        
+        if (! $quote)
+        {
+            $this->_helper->flashMessenger(array (
+                    'warning' => 'Error you must select a quote first.' 
+            ));
+            $this->_helper->redirector('index');
+        }
+        
+        $form = new Quotegen_Form_EditQuote();
+        $form->populate($quote->toArray());
+        
+        $request = $this->getRequest();
+        
+        if ($request->isPost())
+        {
+            $values = $request->getPost();
+            try
+            {
+                if (! isset($values ['cancel']))
+                {
+                    // Create a new instance of quote
+                    $quote = new Quotegen_Model_Quote();
+                    $quote = $this->_quote;
+                    
+                    // Save the new quote
+                    $quoteMapper = Quotegen_Model_Mapper_Quote::getInstance();
+                    $quoteMapper->save($quote, $quote->getId());
+                    
+                    
+                    $this->_helper->flashMessenger(array (
+                            'success' => "Quote updated successfully." 
+                    ));
+                }
+                else
+                {
+                    $this->_helper->redirector('index');
+                }
+            }
+            catch ( Exception $e )
+            {
+                $this->_helper->flashMessenger(array (
+                        'warning' => "There was an issue saving these settings please try again."
+                ));
+            }
+        }
+        
+        $this->view->form = $form;
     }
 }
 
