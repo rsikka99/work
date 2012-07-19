dc493fac4fe8d9b1bfaeb81b996cd95a97aa7a59
Lee Robert <lrobert@tangentmtw.com>
2012-07-19 15:08:41 -0400

    Added a proper admin menu

--- application/modules/admin/Bootstrap.php
+++ application/modules/admin/Bootstrap.php
@@ -11,5 +11,15 @@ class Admin_Bootstrap extends Zend_Application_Module_Bootstrap
         $container = $view->navigation()->getContainer();
         $container->addPages($config);
     }
+    
+    protected function _initAdminNavigation ()
+    {
+        $view = $this->getApplication()->getResource('view');
+        $config = new Zend_Config_Xml(__DIR__ . '/configs/navigation_admin.xml', 'nav');
+        /* @var $container Zend_Navigation */
+        $container = $view->navigation()->getContainer();
+        $systemAdmin = $container->findOneBy('resource', 'admin__index__index');
+        $systemAdmin->addPages($config);
+    }
 }
 
