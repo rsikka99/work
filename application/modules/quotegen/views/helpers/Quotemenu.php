0fc8dd5778974cd71c7f11dd0496627879b6dfd1
Lee Robert <lrobert@tangentmtw.com>
2012-07-20 13:54:42 -0400

    Moved the quote menu into navigation for easier maintenance and rendering.

--- application/modules/quotegen/views/helpers/Quotemenu.php
+++ application/modules/quotegen/views/helpers/Quotemenu.php
@@ -28,12 +28,37 @@ class Quotegen_View_Helper_Quotemenu extends Zend_View_Helper_Abstract
 
     public function Quotemenu ()
     {
-        if (! self::$activePage)
+        // Get the container
+        $html = "";
+        /* @var $pages Zend_Navigation */
+        $pages = $this->view->navigation()->getContainer();
+        $container = $pages->findBy('id', 'quotemenu');
+        if ($container && $container->hasPages())
         {
-            self::$activePage = self::DEVICES_CONTROLLER;
+            // If it's invisible, we'll need to turn it visible to be rendered properly
+            $wasInvisible = false;
+            if (! $container->isVisible())
+            {
+                $wasInvisible = true;
+                $container->setVisible(true);
+            }
+            
+            // Render the menu
+            $html = $this->view->navigation()
+                ->menu()
+                ->renderMenu($container, array (
+                    'minDepth' => 0, 
+                    'maxDepth' => 0, 
+                    'ulClass' => 'nav nav-tabs' 
+            ));
+            
+            // Bring back it's original visibility
+            if ($wasInvisible)
+            {
+                $container->setVisible(false);
+            }
         }
-        $this->view->quoteMenuPages = self::$pages;
-        $this->view->activeQuoteMenuPage = self::$activePage;
-        return $this->view->render('_partials/Quotemenu.phtml');
+        
+        return $html;
     }
 }
