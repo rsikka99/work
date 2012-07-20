0244a55a339b45f5076593c637f43b01d4e1042d
Lee Robert <lrobert@tangentmtw.com>
2012-07-20 15:14:08 -0400

    Added the footer menu to the navigation with a view helper  and restyled it

---
+++ application/layouts/helpers/Footermenu.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Application_View_Helper_Footermenu
+ *
+ * @author Lee Robert
+ *        
+ */
+class Application_View_Helper_Footermenu extends Zend_View_Helper_Abstract
+{
+
+    public function Footermenu ()
+    {
+        // Get the container
+        $html = "";
+        /* @var $pages Zend_Navigation */
+        $pages = $this->view->navigation()->getContainer();
+        $container = $pages->findBy('id', 'footermenu');
+        if ($container && $container->hasPages())
+        {
+            // If it's invisible, we'll need to turn it visible to be rendered properly
+            $wasInvisible = false;
+            if (! $container->isVisible())
+            {
+                $wasInvisible = true;
+                $container->setVisible(true);
+            }
+            
+            // Render the menu
+            $html = $this->view->navigation()
+                ->menu()
+                ->renderMenu($container, array (
+                    'minDepth' => 0, 
+                    'maxDepth' => 0, 
+                    'ulClass' => 'footer-nav' 
+            ));
+            
+            // Bring back it's original visibility
+            if ($wasInvisible)
+            {
+                $container->setVisible(false);
+            }
+        }
+        
+        return $html;
+    }
+}
