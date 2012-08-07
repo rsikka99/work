6846420b2e3b5e35551dad3ad7cdaa6af97a114e
Lee Robert <lrobert@tangentmtw.com>
2012-08-07 11:17:56 -0400

    Updated defined paths to use an application base path to reduce overhead of realpath. Also added an assets path

--- public/index.php
+++ public/index.php
@@ -2,20 +2,21 @@
 ini_set("display_errors", 1);
 error_reporting(E_ALL);
 
-// Define path to application directory
-defined('APPLICATION_PATH') || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
+defined('APPLICATION_BASE_PATH') || define('APPLICATION_BASE_PATH', realpath(dirname(__FILE__) . '/..'));
 
-defined('DATA_PATH') || define('DATA_PATH', realpath(dirname(__FILE__) . '/../data'));
+// Define the paths
+defined('APPLICATION_PATH') || define('APPLICATION_PATH', APPLICATION_BASE_PATH . '/application');
+defined('DATA_PATH') || define('DATA_PATH', APPLICATION_BASE_PATH . '/data');
+defined('PUBLIC_PATH') || define('PUBLIC_PATH', APPLICATION_BASE_PATH . '/public');
+defined('ASSETS_PATH') || define('ASSETS_PATH', APPLICATION_BASE_PATH . '/assets');
 
-defined('PUBLIC_PATH') || define('PUBLIC_PATH', realpath(dirname(__FILE__) . '/../public'));
-
-// Define application environment
+// Define application environment.
 defined('APPLICATION_ENV') || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'development'));
 
 // Ensure library/ is on include_path
 set_include_path(implode(PATH_SEPARATOR, array (
-    realpath(APPLICATION_PATH . '/../library'), 
-    get_include_path() 
+        realpath(APPLICATION_PATH . '/../library'), 
+        get_include_path() 
 )));
 
 /**
