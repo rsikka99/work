fafadcc09a57d31f09c08e895ddf2f54d86d39ea
Lee Robert <lrobert@tangentmtw.com>
2012-08-23 11:25:30 -0400

    Updated inline forms to have 0 margin

--- application/modules/quotegen/forms/AddDevice.php
+++ application/modules/quotegen/forms/AddDevice.php
@@ -7,20 +7,7 @@ class Quotegen_Form_AddDevice extends Twitter_Bootstrap_Form_Inline
     {
         // Set the method for the display form to POST
         $this->setMethod('POST');
-        /**
-         * Add class to form for label alignment
-         *
-         * - Vertical .form-vertical (not required)	Stacked, left-aligned labels
-         * over controls (default)
-         * - Inline .form-inline Left-aligned label and inline-block controls
-         * for compact style
-         * - Search .form-search Extra-rounded text input for a typical search
-         * aesthetic
-         * - Horizontal .form-horizontal
-         *
-         * Use .form-horizontal to have same experience as with Bootstrap v1!
-         */
-        $this->setAttrib('class', 'form-inline');
+        
         
         // An add configuration button for the favorite devices
         $submitButton = $this->createElement('submit', 'addConfiguration', array (
