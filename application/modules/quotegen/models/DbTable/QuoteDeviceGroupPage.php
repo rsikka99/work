bbe4d367bafb4827e73ce6dc1af04bdc9d8fc525
Lee Robert <lrobert@tangentmtw.com>
2012-08-28 12:52:21 -0400

    Removed the page objects as they are no longer used.

--- application/modules/quotegen/models/DbTable/QuoteDeviceGroupPage.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Quotegen_Model_DbTable_QuoteDeviceGroupPage extends Zend_Db_Table_Abstract
-{
-    protected $_name = 'qgen_quote_device_group_pages';
-    protected $_primary = array (
-            'id' 
-    );
-}
