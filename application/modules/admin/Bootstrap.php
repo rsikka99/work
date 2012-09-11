ed6758a961eca0a0f7fea72a4afacdba3335722d
Lee Robert <lrobert@tangentmtw.com>
2012-09-11 09:58:49 -0400

    Updated admin navigation to be placed in the root of the project

--- application/modules/admin/Bootstrap.php
+++ application/modules/admin/Bootstrap.php
@@ -11,15 +11,5 @@ class Admin_Bootstrap extends Zend_Application_Module_Bootstrap
         $container = $view->navigation()->getContainer();
         $container->addPages($config);
     }
-    
-    protected function _initAdminNavigation ()
-    {
-        $view = $this->getApplication()->getResource('view');
-        $config = new Zend_Config_Xml(__DIR__ . '/configs/navigation_admin.xml', 'nav');
-        /* @var $container Zend_Navigation */
-        $container = Zend_Registry::get('Zend_Navigation');
-        $systemAdmin = $container->findOneBy('resource', 'admin__index__index');
-        $systemAdmin->addPages($config);
-    }
 }
 
--- application/modules/proposalgen/Bootstrap.php
+++ application/modules/proposalgen/Bootstrap.php
@@ -12,16 +12,6 @@ class Proposalgen_Bootstrap extends Zend_Application_Module_Bootstrap
         $container->addPages($config);
     }
 
-    protected function _initAdminNavigation ()
-    {
-        $view = $this->getApplication()->getResource('view');
-        $config = new Zend_Config_Xml(__DIR__ . '/configs/navigation_admin.xml', 'nav');
-        /* @var $container Zend_Navigation */
-        $container = Zend_Registry::get('Zend_Navigation');
-        $systemAdmin = $container->findOneBy('resource', 'admin__index__index');
-        $systemAdmin->addPages($config);
-    }
-
     protected function _initAutoloader ()
     {
         $autoloader = Zend_Loader_Autoloader::getInstance();
