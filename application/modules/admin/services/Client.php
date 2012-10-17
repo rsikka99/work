f7ad8d1bf2d03da1074a86064afc0ac76acbf7fd
Tyson Riehl <triehl@tangentmtw.com>
2012-10-17 08:58:25 -0400

    Adds and updates everything to use the new fields

--- application/modules/admin/services/Client.php
+++ application/modules/admin/services/Client.php
@@ -37,18 +37,7 @@ class Admin_Service_Client
         if ($data !== FALSE)
         {
             $client = new Quotegen_Model_Client($data);
-            
-            if (Quotegen_Model_Mapper_Client::getInstance()->findClientByName($client))
-            {
-                $this->getForm()
-                    ->getElement('name')
-                    ->addError('A client with this name already exists');
-                $this->getForm()->buildBootstrapErrorDecorators();
-            }
-            else
-            {
-                $clientId = Quotegen_Model_Mapper_Client::getInstance()->insert($client);
-            }
+            $clientId = Quotegen_Model_Mapper_Client::getInstance()->insert($client);
         }
         
         return $clientId;
@@ -61,53 +50,240 @@ class Admin_Service_Client
      *            An array of data to use when creating a client. The data will be validated using the form.
      * @return boolean Whether or not the update was successful
      */
-    public function update ($data)
+    public function update ($data, $clientId)
     {
         $success = false;
         $data = $this->validateAndFilterData($data);
-        
         if ($data !== FALSE)
         {
             $client = new Quotegen_Model_Client($data);
-            
-            if (Quotegen_Model_Mapper_Client::getInstance()->findClientByName($client))
-            {
-                $this->getForm()
-                    ->getElement('name')
-                    ->addError('A client with this name already exists');
-                $this->getForm()->buildBootstrapErrorDecorators();
-            }
-            else
-            {
-                $rowsAffected = Quotegen_Model_Mapper_Client::getInstance()->save($client);
-                $success = ($rowsAffected);
-            }
+            $client->setId($clientId);
+            $rowsAffected = Quotegen_Model_Mapper_Client::getInstance()->save($client);
+            return 1;
+        }else{
+            return false;
         }
         
-        return $success;
+       // return $success;
+    }
+    
+    /**
+     * Deletes a client from the database where the id is the parameter $id
+     *
+     * @param unknown_type $id
+     */
+    public function delete ($id)
+    {
+        Quotegen_Model_Mapper_Client::getInstance()->delete($id);
     }
 
     /**
+     * Validates the code, changing it if it can to make it work, then returns the new code
+     *
+     * @param unknown_type $code
+     * @return Ambigous <valid, boolean, mixed>|unknown|boolean
+     */
+    protected function validateCode ($code)
+    {
+        $newCode = $this->isValidPostalCode($code);
+        if ($newCode != false)
+        {
+            return $newCode;
+        }
+        else if ($this->isValidZipCode($code))
+        {
+            return $code;
+        }
+        return false;
+    }
+    
+    
+    /**
      * Validates the data with the form
      *
-     * @param array $data
+     * @param array $formData
      *            The array of data to validate
      * @return array The array of valid and filtered data, or false on error.
      */
-    protected function validateAndFilterData ($data)
+    protected function validateAndFilterData ($formData)
     {
-        $validData = false;
+		$valid = true;
         $form = $this->getForm();
+
         
-        if ($form->isValid($data))
+        if (!$form->isValid($formData)){
+            $valid = false;
+        }
+        $validData = $formData;
+        //Make the state always in upper case
+        $validData ['companyStateOrProv'] = strtoupper($validData ['companyStateOrProv']);
+        
+        //create a client using the valid data
+
+        $client = new Quotegen_Model_Client($validData);
+        
+        //set the code to validated code
+        $code = $this->validateCode($client->getCompanyZipOrPostalCode());
+        if (! $code)
         {
-            $validData = $form->getValues();
+            
+            //Postal or Zip code was invalid, display error on form
+            $this->getForm()
+                ->getElement('companyZipOrPostalCode')
+                ->clearErrorMessages()
+                ->addError('Invalid zip or postal code');
+            $valid = false;
         }
         else
         {
-            $this->getForm()->buildBootstrapErrorDecorators();
+            $validData ['companyZipOrPostalCode'] = $code;
         }
-        return $validData;
+        //If it is not a valid state or province, create error messages
+        if (! $this->isValidProvinceOrState($client->getCompanyCountry(), $client->getCompanyStateOrProv()))
+        {
+            $this->getForm()
+                ->getElement('companyStateOrProv')
+                ->clearErrorMessages()
+                ->addError('Invalid state or province');
+            $valid= false;
+        }
+        if($valid)
+        	return $validData;
+        return false;
     }
+    /**
+     * Validates a zip code
+     *
+     * @param $zipcode the
+     *            zip code
+     * @return valid Returns whether it was valid or not
+     *
+     */
+    protected function isValidZipCode ($zipCode)
+    {
+        $postValidator = new Zend_Validate_PostCode('us');
+        if ($postValidator->isValid($zipCode))
+    
+            return true;
+        return false;
+    }
+    
+    /**
+     * Validates a Postal Code
+     *
+     * @param $postalcode the
+     *            postal code
+     * @return valid Returns whether it was valid or not
+     */
+    protected function isValidPostalCode ($postalcode)
+    {
+        $newPostalCode = str_replace(array (
+                '-',
+                ' '
+        ), '', $postalcode);
+        if (preg_match("/^([a-ceghj-npr-tv-z]){1}[0-9]{1}[a-ceghj-npr-tv-z]{1}[0-9]{1}[a-ceghj-npr-tv-z]{1}[0-9]{1}$/i", $newPostalCode))
+            return $newPostalCode;
+        else
+            return false;
+    }
+    
+    /**
+     * Checks to see if the country has the parameter $stateOrProv
+     * @param string $country
+     * @param string $stateOrProv
+     * @return boolean
+     */
+    protected function isValidProvinceOrState ($country, $stateOrProv)
+    {
+        //List of canadian province abbreviations
+        $canada = array (
+                'AB',
+                'BC',
+                'MB',
+                'NB',
+                'NL',
+                'NT',
+                'NS',
+                'NU',
+                'ON',
+                'PE',
+                'QC',
+                'SK',
+                'YT'
+        );
+        //List of american state abreviations
+        $usa = array (
+                "AK",
+                "AL",
+                "AR",
+                "AS",
+                "AZ",
+                "CA",
+                "CO",
+                "CT",
+                "DC",
+                "DE",
+                "FL",
+                "GA",
+                "GU",
+                "HI",
+                "IA",
+                "ID",
+                "IL",
+                "IN",
+                "KS",
+                "KY",
+                "LA",
+                "MA",
+                "MD",
+                "ME",
+                "MH",
+                "MI",
+                "MN",
+                "MO",
+                "MS",
+                "MT",
+                "NC",
+                "ND",
+                "NE",
+                "NH",
+                "NJ",
+                "NM",
+                "NV",
+                "NY",
+                "OH",
+                "OK",
+                "OR",
+                "PA",
+                "PR",
+                "PW",
+                "RI",
+                "SC",
+                "SD",
+                "TN",
+                "TX",
+                "UT",
+                "VA",
+                "VI",
+                "VT",
+                "WA",
+                "WI",
+                "WV",
+                "WY"
+                        );
+                        //determines which country is selected then returns if the code was valid or not
+                        if ($country == 'Canada')
+                        {
+                            $countryValidator = new Zend_Validate_InArray($canada);
+                            return $countryValidator->isValid($stateOrProv);
+                        }
+                        else if ($country == 'United States')
+                        {
+                            $countryValidator = new Zend_Validate_InArray($usa);
+                            return $countryValidator->isValid($stateOrProv);
+                        }
+                        return false;
+    }
+    
 }
 
