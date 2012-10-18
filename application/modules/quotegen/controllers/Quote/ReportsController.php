b99ff94042d073871b47402020f728146b2d3880
Shawn Wilder <swilder@tangentmtw.com>
2012-10-18 08:46:31 -0400

    Removed old code in orderlist view script to accommodate for new view script.  changed the action of the order list icon in the index.phtml file.  Added logic in reports controller to handle new action and being the process of building an excel document

--- application/modules/quotegen/controllers/Quote/ReportsController.php
+++ application/modules/quotegen/controllers/Quote/ReportsController.php
@@ -72,8 +72,147 @@ class Quotegen_Quote_ReportsController extends Quotegen_Library_Controller_Quote
     {
     }
 
+    /**
+     * Action is responsible for create the excel output for the hardware order list
+     */
     public function orderListAction ()
     {
+        $quote = $this->_quote;
+        $totalQuantity = 1;
+        
+        $titles = array (
+                "Item", 
+                "SKU (OEM)", 
+                "SKU (DEALER)", 
+                "Serial Number", 
+                "Location" 
+        );
+        $style_header = array (
+                'fill' => array (
+                        'type' => PHPExcel_Style_Fill::FILL_SOLID, 
+                        'color' => array (
+                                'rgb' => 'E1E0F7' 
+                        ) 
+                ), 
+                'font' => array (
+                        'bold' => true 
+                ) 
+        );
+        $style_header_device = array (
+                'fill' => array (
+                        'type' => PHPExcel_Style_Fill::FILL_SOLID, 
+                        'color' => array (
+                                'rgb' => 'E1E0F7' 
+                        ) 
+                ), 
+                'font' => array (
+                        'bold' => true 
+                ) 
+        );
+        $style_header_titles = array (
+                'fill' => array (
+                        'type' => PHPExcel_Style_Fill::FILL_SOLID, 
+                        'color' => array (
+                                'rgb' => 'E1E0F7' 
+                        ) 
+                ) 
+        );
+        
+        // Create a new PHPExcel Object * New PHPExcel objects will have one attached worksheet by default
+        $objPHPExcel = new PHPExcel();
+        $activeSheet = $objPHPExcel->getActiveSheet();
+        $activeSheet->getStyle('A1:E1')->applyFromArray($style_header);
+        
+        // Ojbects in memory are held in php memory as serializeds objects, reduces memory footpirnt with little performance hit
+        PHPExcel_CachedObjectStorageFactory::cache_in_memory_serialized;
+        
+        // Set properties
+        $objPHPExcel->getProperties()->setTitle("Hardware Device List");
+        
+        // Start building the document
+        $objPHPExcel->setActiveSheetIndex(0);
+        $activeSheet->SetCellValue('A1', 'Hardware Device List');
+        $activeSheet->mergeCells('A1:E1');
+        $activeSheet->getRowDimension('1')->setRowHeight(35);
+        
+        // Go through each device and build and array for output
+        $quoteDeviceGroups = $quote->getQuoteDeviceGroups();
+        /* @var $quoteDeviceGroup Quotegen_Model_QuoteDeviceGroup */
+        foreach ( $quoteDeviceGroups as $quoteDeviceGroup )
+        {
+            /* @var $quoteDeviceGroupDevice Quotegen_Model_QuoteDeviceGroupDevice */
+            foreach ( $quoteDeviceGroup->getQuoteDeviceGroupDevices() as $quoteDeviceGroupDevice )
+            {
+                $quoteDevice = $quoteDeviceGroupDevice->getQuoteDevice();
+                // $device builds an array used to output quote device information
+                $device = array (
+                        $quoteDevice->getName(), 
+                        $quoteDevice->getSku(), 
+                        $quoteDevice->getSku() 
+                );
+                
+                // $deviceOptions builds an array of arrays representing quote device options
+                $deviceOptions = array ();
+                /* @var $quoteDeviceOption Quotegen_Model_QuoteDeviceOption */
+                foreach ( $quoteDeviceGroupDevice->getQuoteDevice()->getQuoteDeviceOptions() as $quoteDeviceOption )
+                {
+                    $deviceOptions [] = array (
+                            "name" => $quoteDeviceOption->getName(), 
+                            "oemSku" => $quoteDeviceOption->getSku(), 
+                            "dealerSku" => $quoteDeviceOption->getSku() 
+                    );
+                }
+                
+                // Build the datasheet
+                // Object->getHighestRow() will return the row index where data has been entered in last.
+                // This as well applies styles that have been defined that the beginning of the funciton
+                for($deviceCount = 1; $deviceCount <= $quoteDeviceGroupDevice->getQuantity(); $deviceCount ++)
+                {
+                    $highestRow = $activeSheet->getHighestRow() + 2;
+                    $activeSheet->setCellValue("A{$highestRow}", "Device {$totalQuantity}");
+                    $activeSheet->mergeCells("A{$highestRow}:E{$highestRow}");
+                    $activeSheet->getStyle("A{$highestRow}:E{$highestRow}")->applyFromArray($style_header_device);
+                    
+                    $highestRow = $activeSheet->getHighestRow() + 1;
+                    
+                    $activeSheet->fromArray($titles, null, "A{$highestRow}");
+                    $activeSheet->getStyle("A{$highestRow}:E{$highestRow}")->applyFromArray($style_header_titles);
+                    
+                    $highestRow = $activeSheet->getHighestRow() + 1;
+                    
+                    $activeSheet->fromArray($device, null, "A{$highestRow}");
+                    
+                    // Use a foreach to go through options instead of using fromArray just in case
+                    // we require to style the option rows.
+                    foreach ( $deviceOptions as $option )
+                    {
+                        $highestRow = $activeSheet->getHighestRow() + 1;
+                        $activeSheet->setCellValue("A{$highestRow}", $option ["name"]);
+                        $activeSheet->setCellValue("B{$highestRow}", $option ["oemSku"]);
+                        $activeSheet->setCellValue("C{$highestRow}", $option ["dealerSku"]);
+                    }
+                    $totalQuantity ++;
+                }
+            }
+        }
+        
+        $activeSheet->getColumnDimension('A')->setAutoSize(true);
+        $activeSheet->getColumnDimension('B')->setAutoSize(true);
+        $activeSheet->getColumnDimension('C')->setAutoSize(true);
+        $activeSheet->getColumnDimension('D')->setAutoSize(true);
+        $activeSheet->getColumnDimension('E')->setAutoSize(true);
+        // Rename sheet
+        $activeSheet->setTitle('Hardware Device List');
+        
+        // Save Excel 2007 file
+        $objWriter = new PHPExcel_Writer_Excel2007($objPHPExcel);
+        $objWriter->save(PUBLIC_PATH . "/downloads/PHPExcel.xlsx");
+        
+        /**
+         * Clearing a workbook from php's memory
+         */
+        $objPHPExcel->disconnectWorksheets();
+        unset($objPHPExcel);
     }
 }
 
