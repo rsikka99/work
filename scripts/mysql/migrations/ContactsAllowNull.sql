b578301f38072be0d5922dcec14e6161e6c1edc6
Lee Robert <lrobert@tangentmtw.com>
2012-11-09 15:06:54 -0500

    Updated migration script to include countrycode being null

--- scripts/mysql/migrations/ContactsAllowNull.sql
+++ scripts/mysql/migrations/ContactsAllowNull.sql
@@ -1,4 +1,5 @@
 ALTER TABLE contacts
+MODIFY COLUMN countryCode smallint(6) NULL,
 MODIFY COLUMN areaCode smallint(6) NULL,
 MODIFY COLUMN exchangeCode smallint(6) NULL,
 MODIFY COLUMN number smallint(6) NULL;
