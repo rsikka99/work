4c95f1ac49fb7573c5b137c476a85ddccf736900
Shawn Wilder <swilder@tangentmtw.com>
2013-01-28 09:42:39 -0500

    MPSTOOLS-25 - Added functionality to use model and mappers for new device_instance_replacement_master_device.

---
+++ application/modules/proposalgen/models/Device/Instance/Replacement/Master/Device.php
@@ -0,0 +1,115 @@
+<?php
+class Proposalgen_Model_Device_Instance_Replacement_Master_Device extends My_Model_Abstract
+{
+    /**
+     * @var int
+     */
+    public $deviceInstanceId;
+
+    /**
+     * @var int
+     */
+    public $masterDeviceId;
+
+    /**
+     * @var Proposalgen_Model_DeviceInstance
+     */
+    protected $_deviceInstance;
+
+    /**
+     * @var Proposalgen_Model_MasterDevice
+     */
+    protected $_masterDevice;
+
+
+    /**
+     * @param array $params An array of data to populate the model with
+     */
+    public function populate ($params)
+    {
+        if (is_array($params))
+        {
+            $params = new ArrayObject($params, ArrayObject::ARRAY_AS_PROPS);
+        }
+
+        if (isset($params->deviceInstanceId) && !is_null($params->deviceInstanceId))
+        {
+            $this->deviceInstanceId = $params->deviceInstanceId;
+        }
+
+        if (isset($params->masterDeviceId) && !is_null($params->masterDeviceId))
+        {
+            $this->masterDeviceId = $params->masterDeviceId;
+        }
+
+    }
+
+    /**
+     * @return array
+     */
+    public function toArray ()
+    {
+        return array(
+            "deviceInstanceId" => $this->deviceInstanceId,
+            "masterDeviceId"   => $this->masterDeviceId,
+        );
+    }
+
+    /**
+     * Gets the device instance
+     *
+     * @return Proposalgen_Model_DeviceInstance
+     */
+    public function getDeviceInstance ()
+    {
+        if (!isset($this->_deviceInstance))
+        {
+            $this->_deviceInstance = Proposalgen_Model_Mapper_DeviceInstance::getInstance()->find($this->deviceInstanceId);
+        }
+
+        return $this->_deviceInstance;
+    }
+
+    /**
+     *  Sets the device instance
+     *
+     * @param Proposalgen_Model_DeviceInstance $deviceInstance
+     *
+     * @return Proposalgen_Model_Device_Instance_Master_Device
+     */
+    public function setDeviceInstance ($deviceInstance)
+    {
+        $this->_deviceInstance = $deviceInstance;
+
+        return $this;
+    }
+
+    /**
+     * Gets the master device
+     *
+     * @return Proposalgen_Model_MasterDevice
+     */
+    public function getMasterDevice ()
+    {
+        if (!isset($this->_masterDevice))
+        {
+            $this->_masterDevice = Proposalgen_Model_Mapper_MasterDevice::getInstance()->find($this->masterDeviceId);
+        }
+
+        return $this->_masterDevice;
+    }
+
+    /**
+     *  Sets the master device
+     *
+     * @param Proposalgen_Model_MasterDevice $masterDevice
+     *
+     * @return Proposalgen_Model_Device_Instance_Master_Device
+     */
+    public function setMasterDevice ($masterDevice)
+    {
+        $this->_masterDevice = $masterDevice;
+
+        return $this;
+    }
+}
