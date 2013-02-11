9140d63eda0fef4eacea4cf1d23d8a41b16bee4e
Lee Robert <lrobert@tangentmtw.com>
2013-02-11 15:53:15 -0500

    MPSTOOLS-40 - First iteration of the new landing page

--- application/modules/default/controllers/IndexController.php
+++ application/modules/default/controllers/IndexController.php
@@ -1,32 +1,82 @@
 <?php
 class Default_IndexController extends Zend_Controller_Action
 {
+    /**
+     * @var int
+     */
+    protected $_selectedClientId;
+
+    /**
+     * The namespace for our mps application
+     *
+     * @var Zend_Session_Namespace
+     */
+    protected $_mpsSession;
 
     public function init ()
     {
         /* Initialize action controller here */
+        $this->_mpsSession = new Zend_Session_Namespace('mps-tools');
+        if (isset($this->_mpsSession->selectedClientId))
+        {
+            $this->_selectedClientId      = $this->_mpsSession->selectedClientId;
+            $this->view->selectedClientId = $this->_selectedClientId;
+        }
     }
 
+    /**
+     * Main landing page
+     */
     public function indexAction ()
     {
-        // action body
-//        My_Log::log('Sample Log (DefaultModule - IndexController - IndexAction): Logging Started', Zend_Log::INFO);
-//        My_Log::log('Sample Log (DefaultModule - IndexController - IndexAction): Candying that bacon.', Zend_Log::INFO);
-//        My_Log::log('Sample Log (DefaultModule - IndexController - IndexAction): User is not authenticated!', Zend_Log::INFO);
-//        My_Log::log('Sample Log (DefaultModule - IndexController - IndexAction): User has 4 login attempts left.', Zend_Log::INFO);
+        if ($this->_selectedClientId === null)
+        {
+            $this->_helper->redirector('select-client');
+        }
 
-//        if (Zend_Auth::getInstance()->hasIdentity())
-//        {
-//            $userId = Zend_Auth::getInstance()->getIdentity()->id;
-//            $acl = Application_Model_Acl::getInstance();
-//
-//            if ($acl->isAllowed($userId, "default_index_index", Application_Model_Acl::PRIVILEGE_VIEW))
-//            {
-//                echo "<pre>Var dump initiated at " . __LINE__ . " of:\n" . __FILE__ . "\n\n";
-//                var_dump("User has view level privileges");
-//                die();
-//            }
-//        }
+        if ($this->getRequest()->isPost())
+        {
+            $postData = $this->getRequest()->getPost();
 
+            /**
+             * Change clients
+             */
+            if (isset($postData['changeClient']))
+            {
+                unset($this->_mpsSession->selectedClientId);
+                $this->_helper->redirector('select-client');
+            }
+        }
+
+    }
+
+    /**
+     * Allows a user to select a client to work with
+     */
+    public function selectClientAction ()
+    {
+        $clientId     = $this->_getParam('clientId', false);
+        $clientMapper = Quotegen_Model_Mapper_Client::getInstance();
+        if ($clientId !== false)
+        {
+            /**
+             * Here we reset our selected client if the id is 0
+             */
+            if ((int)$clientId === 0)
+            {
+                unset($this->_mpsSession->selectedClientId);
+            }
+            else
+            {
+                $client = $clientMapper->find($clientId);
+                if ($client instanceof Quotegen_Model_Client)
+                {
+                    $this->_mpsSession->selectedClientId = $clientId;
+                    $this->_helper->redirector('index');
+                }
+            }
+        }
+        $clients             = $clientMapper->fetchAll(null, null, 1000, null);
+        $this->view->clients = $clients;
     }
 }
