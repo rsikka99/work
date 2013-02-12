afc71048a6b90eb6d9edbd334a35b485d4ce14a1
Shawn Wilder <swilder@tangentmtw.com>
2013-02-12 12:39:14 -0500

    MPSTOOLS-25 - Added two more charts onto the hardware optimization.

--- application/modules/proposalgen/models/Optimization/Abstract.php
+++ application/modules/proposalgen/models/Optimization/Abstract.php
@@ -7,14 +7,12 @@ abstract class Proposalgen_Model_Optimization_Abstract
      * @var Proposalgen_Model_DeviceInstance []
      */
     public $replaced;
-
     /**
      * Deices that have their action flagged as retire
      *
      * @var Proposalgen_Model_DeviceInstance []
      */
     public $retired;
-
     /**
      * Devices that are either replacement devices that haven't been replacement, or good devices that haven't been
      * replaced
@@ -22,28 +20,24 @@ abstract class Proposalgen_Model_Optimization_Abstract
      * @var Proposalgen_Model_DeviceInstance []
      */
     public $kept;
-
     /**
      * Devices that have been swapped out
      *
      * @var Proposalgen_Model_DeviceInstance []
      */
     public $excess;
-
     /**
      * Devices that have been excluded from the original reporting
      *
      * @var Proposalgen_Model_DeviceInstance []
      */
     public $excluded;
-
     /**
      * Devices that are leased
      *
      * @var Proposalgen_Model_DeviceInstance  []
      */
     public $leased;
-
     /**
      * Devices that action is replace, with no replacement devices assigned
      *
@@ -68,45 +62,92 @@ abstract class Proposalgen_Model_Optimization_Abstract
      * @var number
      */
     public $actionRetireCount;
-
     /**
      * Gets the amount of purchased devices that are JIT Compatible
      *
      * @var int
      */
     public $jitCompatibleCount;
-
     /**
      * Stores the proposal object for future use
      *
      * @var Proposalgen_Model_Proposal_OfficeDepot
      */
     public $proposal;
+    /**
+     * Stores count of devices based on age ranking
+     *
+     * @var array
+     */
+    public $deviceAges = array();
+
+    /**
+     * Stores the count of devices based on categories required
+     *
+     * @var array
+     */
+    public $deviceCategories = array();
 
     public function __construct (Proposalgen_Model_Proposal_OfficeDepot $proposal)
     {
         $this->proposal = $proposal;
         // Set up the arrays of devices to be produced
-        $retiredDevices      = array();
-        $replacedDevices     = array();
-        $keepDevices         = array();
-        $excessDevices       = array();
-        $flaggedDevices      = array();
-        $actionKeep          = 0;
-        $actionReplace       = 0;
-        $actionRetire        = 0;
+        $retiredDevices           = array();
+        $replacedDevices          = array();
+        $keepDevices              = array();
+        $excessDevices            = array();
+        $flaggedDevices           = array();
+        $actionKeep               = 0;
+        $actionReplace            = 0;
+        $actionRetire             = 0;
         $this->jitCompatibleCount = 0;
 
-        // Go through each purchase devices that rank it's classifictions
+        // Set up the age ranks to get devices that are greater than the values in the array
+        $ageRanks = array("8", "4", "2", "0");
+        // Initialize the values for each age rank
+        foreach ($ageRanks as $ageRank)
+        {
+            $this->deviceAges[$ageRank] = 0;
+        }
+
+        // Initialize the categories variables count to 0
+        $this->deviceCategories["copier"] = 0;
+        $this->deviceCategories["scanner"] = 0;
+        $this->deviceCategories["duplex"] = 0;
+
+        // Go through each purchase devices that rank it's classifications
         /* @var $deviceInstance Proposalgen_Model_DeviceInstance */
         foreach ($proposal->getPurchasedDevices() as $deviceInstance)
         {
+            // For each age rank, check if device is greater than range.
+            foreach ($ageRanks as $ageRank)
+            {
+                if ($deviceInstance->getMasterDevice()->getAge() > $ageRank)
+                {
+                    $this->deviceAges[$ageRank]++;
+                    break;
+                }
+            }
+
+            if ($deviceInstance->getMasterDevice()->isCopier)
+            {
+                $this->deviceCategories["copy"]++;
+            }
+            if ($deviceInstance->getMasterDevice()->isScanner)
+            {
+                $this->deviceCategories["scan"]++;
+            }
+            if ($deviceInstance->getMasterDevice()->isDuplex)
+            {
+                $this->deviceCategories["duplex"]++;
+            }
 
             // Checks to see if the device is JIT Compatible
-            if($deviceInstance->getMasterDevice()->reportsTonerLevels)
+            if ($deviceInstance->getMasterDevice()->reportsTonerLevels)
             {
                 $this->jitCompatibleCount++;
             }
+
             // Check the action status first, then check the replacement status
             if ($deviceInstance->getAction() === Proposalgen_Model_DeviceInstance::ACTION_REPLACE)
             {
@@ -141,7 +182,6 @@ abstract class Proposalgen_Model_Optimization_Abstract
                 $keepDevices [] = $deviceInstance;
             }
         }
-
 
         $leasedDevices   = $proposal->getLeasedDevices();
         $excludedDevices = $proposal->getExcludedDevices();
--- application/modules/proposalgen/models/Optimization/Customer.php
+++ application/modules/proposalgen/models/Optimization/Customer.php
@@ -7,20 +7,23 @@ class Proposalgen_Model_Optimization_Customer extends
      *
      * @var string []
      */
-    protected  $_graphs;
+    protected $_graphs;
 
     public function getGraphs ()
     {
         if (!isset($this->graphs))
         {
-            $proposalGraphs = $this->proposal->getGraphs();
+            $proposalGraphs      = $this->proposal->getGraphs();
             $purchaseDeviceCount = count($this->proposal->getPurchasedDevices());
 
             // N = number, p = percent, 0 = number of decimal places.
             // p will take 0.1544 as a number and convert it to 15%
-            $numberValueMarker = "N*p0";
-            /* Hardware Optimization Device Summary Listing (before hardware optimization */
-            $graph   = new gchart\gBarChart(520, 320, "g", "h");
+            $percentValueMarker = "N*p0";
+            $numberValueMarker = "N *sz0";
+            /**
+             * -- Hardware Optimization Device Summary Listing (before hardware optimization
+             */
+            $graph = new gchart\gBarChart(520, 320, "g", "h");
             $graph->setVisibleAxes(array('x'));
             // Amount of keep devices
             $graph->addDataSet(array($this->actionKeepCount / $purchaseDeviceCount));
@@ -43,15 +46,16 @@ class Proposalgen_Model_Optimization_Customer extends
                                    "Replaced Devices",
                               ));
             $graph->setTitle("Current Fleet Summary");
-            $graph->addValueMarkers($numberValueMarker, "000000", "0", "-1", "11");
-            $graph->addValueMarkers($numberValueMarker, "000000", "1", "-1", "11");
-            $graph->addValueMarkers($numberValueMarker, "000000", "2", "-1", "11");
-            $graph->addValueMarkers($numberValueMarker, "000000", "3", "-1", "11");
+            $graph->addValueMarkers($percentValueMarker, "000000", "0", "-1", "11");
+            $graph->addValueMarkers($percentValueMarker, "000000", "1", "-1", "11");
+            $graph->addValueMarkers($percentValueMarker, "000000", "2", "-1", "11");
+            $graph->addValueMarkers($percentValueMarker, "000000", "3", "-1", "11");
             $this->_graphs [] = $graph->getUrl();
 
 
-            // Hardware Optimization Device Summary Listing
-            $highest = ($this->actionKeepCount / $purchaseDeviceCount);
+            /**
+             * -- Hardware Optimization Device Summary Listing
+             */
             $graph   = new gchart\gBarChart(520, 320, "g", "h");
             $graph->setVisibleAxes(array('x'));
             $graph->addDataSet(array((count($this->kept) / $purchaseDeviceCount)));
@@ -72,10 +76,10 @@ class Proposalgen_Model_Optimization_Customer extends
                                    "Replaced Devices",
                               ));
             $graph->setTitle("Optimized Fleet Summary");
-            $graph->addValueMarkers($numberValueMarker, "000000", "0", "-1", "11");
-            $graph->addValueMarkers($numberValueMarker, "000000", "1", "-1", "11");
-            $graph->addValueMarkers($numberValueMarker, "000000", "2", "-1", "11");
-            $graph->addValueMarkers($numberValueMarker, "000000", "3", "-1", "11");
+            $graph->addValueMarkers($percentValueMarker, "000000", "0", "-1", "11");
+            $graph->addValueMarkers($percentValueMarker, "000000", "1", "-1", "11");
+            $graph->addValueMarkers($percentValueMarker, "000000", "2", "-1", "11");
+            $graph->addValueMarkers($percentValueMarker, "000000", "3", "-1", "11");
             $this->_graphs [] = $graph->getUrl();
 
             $this->_graphs [] = $proposalGraphs[1];
@@ -89,10 +93,11 @@ class Proposalgen_Model_Optimization_Customer extends
             /**
              * -- Printing List JIT Compatible
              */
-            $jitCompatible = $this->jitCompatibleCount;
+
+            $jitCompatible    = $this->jitCompatibleCount;
             $nonJitCompatible = count($this->proposal->getPurchasedDevices()) - $this->jitCompatibleCount;
-            $highest            = ($jitCompatible > $nonJitCompatible) ? $jitCompatible : $nonJitCompatible;
-            $barGraph           = new gchart\gBarChart(200, 300);
+            $highest          = ($jitCompatible > $nonJitCompatible) ? $jitCompatible : $nonJitCompatible;
+            $barGraph         = new gchart\gBarChart(200, 300);
             $barGraph->setTitle(" JIT Compatibility");
             $barGraph->setVisibleAxes(array(
                                            'y'
@@ -124,24 +129,44 @@ class Proposalgen_Model_Optimization_Customer extends
             $this->_graphs [] = $proposalGraphs[7];
             $this->_graphs [] = $proposalGraphs[8];
 
-            // Placeholder for graph
-            $this->_graphs [] = $proposalGraphs[0];
-
-
+            /**
+             * -- Categories of technology features
+             */
+            $barGraph         = new gchart\gBarChart(200, 300);
+            $barGraph->setTitle(" Green Features");
+            $barGraph->setVisibleAxes(array('y'));
+            $barGraph->addDataSet(array($this->deviceCategories["copy"] / $purchaseDeviceCount));
+            $barGraph->addColors(array("E21736"));
+            $barGraph->addDataSet(array($this->deviceCategories["duplex"] / $purchaseDeviceCount));
+            $barGraph->addColors(array("A21736"));
+            $barGraph->addDataSet(array($this->deviceCategories["scan"] / $purchaseDeviceCount));
+            $barGraph->addColors(array("6277A6"));
+            $barGraph->setDataRange(0, 1);
+            $barGraph->setBarScale(40, 10);
+            $barGraph->setLegendPosition("bv");
+            $barGraph->addColors(array(
+                                      "0194D2"
+                                 ));
+            $barGraph->setLegend(array(
+                                      "Copy Capable ",
+                                      "Duplex Capable",
+                                      "Scan Capable"
+                                 ));
+            $barGraph->addValueMarkers($percentValueMarker, "000000", "0", "-1", "11");
+            $barGraph->addValueMarkers($percentValueMarker, "000000", "1", "-1", "11");
+            $barGraph->addValueMarkers($percentValueMarker, "000000", "2", "-1", "11");
+            $this->_graphs [] = $barGraph->getUrl();
 
             /**
              * -- Age vertical bar chart
              */
-//            $jitCompatible = $this->jitCompatibleCount;
-//            $nonJitCompatible = count($this->proposal->getPurchasedDevices()) - $this->jitCompatibleCount;
-            $numberValueMarker = "N *sz0";
-            $highest            = ($jitCompatible > $nonJitCompatible) ? $jitCompatible : $nonJitCompatible;
-            $barGraph           = new gchart\gBarChart(400, 300);
+            $highest           = ($jitCompatible > $nonJitCompatible) ? $jitCompatible : $nonJitCompatible;
+            $barGraph          = new gchart\gBarChart(200, 200);
             $barGraph->setTitle("Age of printing devices");
             $barGraph->setVisibleAxes(array('x'));
 
-            $barGraph->addDataSet(array(30,44,49,433));
-            $barGraph->addColors(array("E21736","A21746","12AB36","A2CB36"));
+            $barGraph->addDataSet(array($this->deviceAges[0], $this->deviceAges[2], $this->deviceAges[4], $this->deviceAges[8]));
+            $barGraph->addColors(array("E21736", "A21746", "12AB36", "A2CB36"));
 
             $barGraph->setDataRange(0, $highest * 1.1);
             $barGraph->setBarScale(40, 20);
@@ -150,7 +175,7 @@ class Proposalgen_Model_Optimization_Customer extends
             $barGraph->addValueMarkers($numberValueMarker, "000000", "1", "-1", "11");
             $barGraph->addValueMarkers($numberValueMarker, "000000", "2", "-1", "11");
             $barGraph->addValueMarkers($numberValueMarker, "000000", "3", "-1", "11");
-            $barGraph->addAxisLabel(0,array("100","3","69","86"));
+            $barGraph->addAxisLabel(0, array("0-2", "2-4", "4-8", "8+"));
             $this->_graphs [] = $barGraph->getUrl();
             $this->_graphs [] = $proposalGraphs[0];
 
