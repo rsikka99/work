60b941f869cf40201e8035c87135f3842b91c09b
Shawn Wilder <swilder@tangentmtw.com>
2013-02-19 09:17:48 -0500

    MPTOOSL-25 - Created the ability to generate a word document of dealer hardware optimization based on the current state in r4.

--- application/modules/proposalgen/controllers/Report/Optimization/DealerController.php
+++ application/modules/proposalgen/controllers/Report/Optimization/DealerController.php
@@ -12,7 +12,7 @@ class Proposalgen_Report_Optimization_DealerController extends Proposalgen_Libra
         $this->view->availableReports->DealerHardwareOptimization->active = true;
 
         $this->view->formats = array(
-            "/proposalgen/solution/generate/format/docx" => $this->_wordFormat
+            "/proposalgen/report_optimization_dealer/generate/format/docx" => $this->_wordFormat
         );
 
         try
@@ -29,5 +29,44 @@ class Proposalgen_Report_Optimization_DealerController extends Proposalgen_Libra
         }
 
         $this->_helper->layout->setLayout('htmlreport');
+    }
+
+    /**
+     * The Index action of the solution.
+     */
+    public function generateAction ()
+    {
+        $format = $this->_getParam("format", "docx");
+
+        switch ($format)
+        {
+            case "csv" :
+                throw new Exception("CSV Format not available through this page yet!");
+                break;
+            case "docx" :
+                require_once ('PHPWord.php');
+                $this->view->phpword = new PHPWord();
+                $this->_helper->layout->disableLayout();
+                break;
+            case "pdf" :
+                throw new Exception("PDF Format not available through this page yet!");
+                break;
+            default :
+                throw new Exception("Invalid Format Requested!");
+                break;
+        }
+
+        $filename = "dealerHardwareOptimization.$format";
+
+        $this->initReportVariables($filename);
+        // Render early
+        try
+        {
+            $this->render($format . "/00_render");
+        }
+        catch (Exception $e)
+        {
+            throw new Exception("Controller caught the exception!", 0, $e);
+        }
     }
 }
