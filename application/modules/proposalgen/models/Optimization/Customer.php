54efc048c747725f2b62cc8ff83ab3b09329d29d
Shawn Wilder <swilder@tangentmtw.com>
2013-02-27 09:57:28 -0500

    MPSTOOLS-25 - Put charts in order of get graphs function call .

--- application/modules/proposalgen/models/Optimization/Customer.php
+++ application/modules/proposalgen/models/Optimization/Customer.php
@@ -19,52 +19,15 @@ class Proposalgen_Model_Optimization_Customer extends
             // N = number, p = percent, 0 = number of decimal places.
             // p will take 0.1544 as a number and convert it to 15%
             $percentValueMarker = "N  *p0";
-            $numberValueMarker = "N *sz0";
-            /**
-             * -- Hardware Optimization Device Summary Listing (before hardware optimization
-             */
-            $graph = new gchart\gBarChart(280, 350, "g", "h");
-            $graph->setVisibleAxes(array('x'));
-            // Amount of keep devices
-
-            $graph->addDataSet(array($this->actionKeepCount / $purchaseDeviceCount));
-            $graph->addColors(array("0CC2D2"));
-            $percentValueMarker1 = "N  *p0* ({$this->actionKeepCount})";
-            // Set the replaced amount to 0
-            $graph->addDataSet(array(0));
-            $graph->addColors(array("9bbb59"));
-            $percentValueMarker2 = "N  *p0* (0)";
-
-            // Amount of replace devices
-            $graph->addDataSet(array($this->actionReplaceCount / $purchaseDeviceCount));
-            $graph->addColors(array("4f81bd"));
-            $percentValueMarker3 = "N  *p0* ({$this->actionReplaceCount})";
-            // Amount of retire devices
-            $graph->addDataSet(array($this->actionRetireCount / $purchaseDeviceCount));
-            $graph->addColors(array("c0504d"));
-            $percentValueMarker4 = "N  *p0* ({$this->actionRetireCount})";
-
-
-            $graph->setDataRange(0, 1);
-            $graph->setBarScale(50, 10);
-            $graph->setLegendPosition("b");
-            $graph->setLegend(array(
-                                   "Keep",
-                                   "Replace",
-                                   "Flagged",
-                                   "Retire",
-                              ));
-            $graph->setTitle("Current Fleet Summary");
-            $graph->addValueMarkers($percentValueMarker1, "000000", "0", "-1", "11");
-            $graph->addValueMarkers($percentValueMarker2, "000000", "1", "-1", "11");
-            $graph->addValueMarkers($percentValueMarker3, "000000", "2", "-1", "11");
-            $graph->addValueMarkers($percentValueMarker4, "000000", "3", "-1", "11");
-            $this->_graphs [] = $graph->getUrl();
+            $numberValueMarker  = "N *sz0";
+            $this->_graphs []   = $proposalGraphs[4];
+            $this->_graphs []   = $proposalGraphs[5];
+            $this->_graphs []   = $proposalGraphs[2];
 
             /**
              * -- Hardware Optimization Device Summary Listing
              */
-            $graph   = new gchart\gBarChart(425, 325, "g", "h");
+            $graph = new gchart\gBarChart(425, 325, "g", "h");
             $graph->setVisibleAxes(array('x'));
 
             $count = count($this->kept);
@@ -103,114 +66,9 @@ class Proposalgen_Model_Optimization_Customer extends
             $graph->addValueMarkers($percentValueMarker4, "000000", "3", "-1", "11");
             $this->_graphs [] = $graph->getUrl();
 
-            $this->_graphs [] = $proposalGraphs[1];
-            $this->_graphs [] = $proposalGraphs[2];
-            $this->_graphs [] = $proposalGraphs[3];
-            $this->_graphs [] = $proposalGraphs[4];
-            $this->_graphs [] = $proposalGraphs[5];
-            $this->_graphs [] = $proposalGraphs[6];
-
-
-            /**
-             * -- Printing List JIT Compatible
-             */
-
-            $jitCompatible    = $this->jitCompatibleCount;
-            $nonJitCompatible = count($this->proposal->getPurchasedDevices()) - $this->jitCompatibleCount;
-            $highest          = ($jitCompatible > $nonJitCompatible) ? $jitCompatible : $nonJitCompatible;
-            $barGraph         = new gchart\gBarChart(200, 300);
-            $barGraph->setTitle(" JIT Compatibility");
-            $barGraph->setVisibleAxes(array(
-                                           'y'
-                                      ));
-            $barGraph->addDataSet(array(
-                                       $jitCompatible
-                                  ));
-            $barGraph->addColors(array(
-                                      "E21736"
-                                 ));
-            $barGraph->addDataSet(array(
-                                       $nonJitCompatible
-                                  ));
-            $barGraph->addAxisRange(0, 0, $highest * 1.1);
-            $barGraph->setDataRange(0, $highest * 1.1);
-            $barGraph->setBarScale(40, 10);
-            $barGraph->setLegendPosition("bv");
-            $barGraph->addColors(array(
-                                      "0194D2"
-                                 ));
-            $barGraph->setLegend(array(
-                                      "JIT Compatible ",
-                                      "Non JIT Compatible "
-                                 ));
-            $barGraph->addValueMarkers($numberValueMarker, "000000", "0", "-1", "11");
-            $barGraph->addValueMarkers($numberValueMarker, "000000", "1", "-1", "11");
-            $this->_graphs [] = $barGraph->getUrl();
-
-            $this->_graphs [] = $proposalGraphs[7];
-            $this->_graphs [] = $proposalGraphs[8];
-
-            /**
-             * -- Age vertical bar chart
-             */
-            $highest           = ($this->deviceAges[0] > $this->deviceAges[2]) ? $this->deviceAges[0] : $this->deviceAges[2];
-            $highest           = ($this->deviceAges[4] > $highest) ? $this->deviceAges[4] : $highest;
-            $highest           = ($this->deviceAges[8] > $highest) ? $this->deviceAges[8] : $highest;
-            $barGraph          = new gchart\gBarChart(375, 250);
-            $barGraph->setTitle("Age of printing devices");
-            $barGraph->setVisibleAxes(array('y'));
-
-            $colors = array("E21736", "A21746", "12AB36", "A2CB36");
-            foreach ($colors as $color)
-            {
-                $barGraph->addColors(array($color));
-            }
-            foreach (array_reverse($this->deviceAges) as $deviceCount)
-            {
-                $barGraph->addDataSet(array($deviceCount));
-            }
-            $barGraph->setLegend(array_reverse(Proposalgen_Model_Optimization_Abstract::$ageRanks));
-            $barGraph->setLegendPosition("b");
-            $barGraph->addAxisRange(0, 0, $highest * 1.1);
-            $barGraph->setDataRange(0, $highest * 1.1);
-            $barGraph->setBarScale(65, 10);
-            $barGraph->addValueMarkers($numberValueMarker, "000000", "0", "-1", "11");
-            $barGraph->addValueMarkers($numberValueMarker, "000000", "1", "-1", "11");
-            $barGraph->addValueMarkers($numberValueMarker, "000000", "2", "-1", "11");
-            $barGraph->addValueMarkers($numberValueMarker, "000000", "3", "-1", "11");
-            $this->_graphs [] = $barGraph->getUrl();
-
-            /**
-             * -- Categories of technology features
-             */
-            $barGraph         = new gchart\gBarChart(375, 300);
-            $barGraph->setTitle(" Green Features");
-            $barGraph->setVisibleAxes(array('y'));
-            $barGraph->addDataSet(array($this->deviceCategories["copy"] / $purchaseDeviceCount));
-            $barGraph->addColors(array("E21736"));
-            $barGraph->addDataSet(array($this->deviceCategories["duplex"] / $purchaseDeviceCount));
-            $barGraph->addColors(array("A21736"));
-            $barGraph->addDataSet(array($this->deviceCategories["scan"] / $purchaseDeviceCount));
-            $barGraph->addColors(array("6277A6"));
-            $barGraph->setDataRange(0, 1);
-            $barGraph->setBarScale(100, 10);
-            $barGraph->setLegendPosition("b");
-            $barGraph->addColors(array(
-                                      "0194D2"
-                                 ));
-            $barGraph->setLegend(array(
-                                      "Copy Capable ",
-                                      "Duplex Capable",
-                                      "Scan Capable"
-                                 ));
-            $barGraph->addValueMarkers($percentValueMarker, "000000", "0", "-1", "11");
-            $barGraph->addValueMarkers($percentValueMarker, "000000", "1", "-1", "11");
-            $barGraph->addValueMarkers($percentValueMarker, "000000", "2", "-1", "11");
-            $this->_graphs [] = $barGraph->getUrl();
-
             /**
              * -- Hardware Utilization and Capacity Percent version
-            */
+             */
             $percentage = ($this->proposal->getPageCounts()->Total->Combined->Monthly / $this->proposal->getMaximumMonthlyPrintVolume());
 
             $highest  = 100;
@@ -255,10 +113,10 @@ class Proposalgen_Model_Optimization_Customer extends
             $barGraph->setDataRange(0, $highest);
             $barGraph->setBarScale(40, 10);
             $barGraph->setLegendPosition("bv");
-            $dotProperties = '@d,E21736,0,.5:' . number_format($percentage,2) . ',23|';
+            $dotProperties = '@d,E21736,0,.5:' . number_format($percentage, 2) . ',23|';
 //            $barGraph->setProperty('chm',$dotProperties);
-            $dotProperties .= '@t' . number_format($percentage * 100,2) . '%,000000,0,-2.5:' . number_format($percentage - .03,2)  . ',10';
-            $barGraph->setProperty('chm',$dotProperties);
+            $dotProperties .= '@t' . number_format($percentage * 100, 2) . '%,000000,0,-2.5:' . number_format($percentage - .03, 2) . ',10';
+            $barGraph->setProperty('chm', $dotProperties);
 //            $dotProperties = '000000,FF0000|00FF00|0000FF';
 //            $barGraph->setProperty('chco',$dotProperties);
             $barGraph->addColors(array(
@@ -269,11 +127,11 @@ class Proposalgen_Model_Optimization_Customer extends
             $this->_graphs [] = $barGraph->getUrl();
 
             $percentage = ($this->proposal->getPageCounts()->Total->Combined->Monthly / $this->proposal->calculateMaximumMonthlyPrintVolumeWithReplacements());
-            $highest  = 100;
-            $barGraph = new gchart\gStackedBarChart(600, 160);
+            $highest    = 100;
+            $barGraph   = new gchart\gStackedBarChart(600, 160);
             $barGraph->setHorizontal(true);
             $barGraph->setVisibleAxes(array(
-                                           'x','y'
+                                           'x', 'y'
                                       ));
             $barGraph->addDataSet(array(
                                        0
@@ -311,10 +169,10 @@ class Proposalgen_Model_Optimization_Customer extends
             $barGraph->setDataRange(0, $highest);
             $barGraph->setBarScale(40, 10);
             $barGraph->setLegendPosition("bv");
-            $dotProperties = '@d,E21736,0,.5:' . number_format($percentage,2) . ',23|';
+            $dotProperties = '@d,E21736,0,.5:' . number_format($percentage, 2) . ',23|';
 //            $barGraph->setProperty('chm',$dotProperties);
-            $dotProperties .= '@t' . number_format($percentage * 100,2) . '%,000000,0,-4.5:' . number_format($percentage - .03,2)  . ',10,,:0:110';
-            $barGraph->setProperty('chm',$dotProperties);
+            $dotProperties .= '@t' . number_format($percentage * 100, 2) . '%,000000,0,-4.5:' . number_format($percentage - .03, 2) . ',10,,:0:110';
+            $barGraph->setProperty('chm', $dotProperties);
 //            $dotProperties = '000000,FF0000|00FF00|0000FF';
 //            $barGraph->setProperty('chco',$dotProperties);
             $barGraph->addColors(array(
@@ -323,7 +181,108 @@ class Proposalgen_Model_Optimization_Customer extends
             $barGraph->setProperty('chxs', '0N*sz0*');
 //            $barGraph->addValueMarkers($numberValueMarker, "000000", "0", "-1", "11");
             $this->_graphs [] = $barGraph->getUrl();
+
+            $this->_graphs [] = $proposalGraphs[6];
+
+
+            /**
+             * -- Printing List JIT Compatible
+             */
+
+            $jitCompatible    = $this->jitCompatibleCount;
+            $nonJitCompatible = count($this->proposal->getPurchasedDevices()) - $this->jitCompatibleCount;
+            $highest          = ($jitCompatible > $nonJitCompatible) ? $jitCompatible : $nonJitCompatible;
+            $barGraph         = new gchart\gBarChart(200, 300);
+            $barGraph->setTitle(" JIT Compatibility");
+            $barGraph->setVisibleAxes(array(
+                                           'y'
+                                      ));
+            $barGraph->addDataSet(array(
+                                       $jitCompatible
+                                  ));
+            $barGraph->addColors(array(
+                                      "E21736"
+                                 ));
+            $barGraph->addDataSet(array(
+                                       $nonJitCompatible
+                                  ));
+            $barGraph->addAxisRange(0, 0, $highest * 1.1);
+            $barGraph->setDataRange(0, $highest * 1.1);
+            $barGraph->setBarScale(40, 10);
+            $barGraph->setLegendPosition("bv");
+            $barGraph->addColors(array(
+                                      "0194D2"
+                                 ));
+            $barGraph->setLegend(array(
+                                      "JIT Compatible ",
+                                      "Non JIT Compatible "
+                                 ));
+            $barGraph->addValueMarkers($numberValueMarker, "000000", "0", "-1", "11");
+            $barGraph->addValueMarkers($numberValueMarker, "000000", "1", "-1", "11");
+            $this->_graphs [] = $barGraph->getUrl();
+            $this->_graphs [] = $proposalGraphs[1];
+            /**
+             * -- Categories of technology features
+             */
+            $barGraph = new gchart\gBarChart(375, 300);
+            $barGraph->setTitle(" Green Features");
+            $barGraph->setVisibleAxes(array('y'));
+            $barGraph->addDataSet(array($this->deviceCategories["copy"] / $purchaseDeviceCount));
+            $barGraph->addColors(array("E21736"));
+            $barGraph->addDataSet(array($this->deviceCategories["duplex"] / $purchaseDeviceCount));
+            $barGraph->addColors(array("A21736"));
+            $barGraph->addDataSet(array($this->deviceCategories["scan"] / $purchaseDeviceCount));
+            $barGraph->addColors(array("6277A6"));
+            $barGraph->setDataRange(0, 1);
+            $barGraph->setBarScale(100, 10);
+            $barGraph->setLegendPosition("b");
+            $barGraph->addColors(array(
+                                      "0194D2"
+                                 ));
+            $barGraph->setLegend(array(
+                                      "Copy Capable ",
+                                      "Duplex Capable",
+                                      "Scan Capable"
+                                 ));
+            $barGraph->addValueMarkers($percentValueMarker, "000000", "0", "-1", "11");
+            $barGraph->addValueMarkers($percentValueMarker, "000000", "1", "-1", "11");
+            $barGraph->addValueMarkers($percentValueMarker, "000000", "2", "-1", "11");
+            $this->_graphs [] = $barGraph->getUrl();
+
+            /**
+             * -- Age vertical bar chart
+             */
+            $highest  = ($this->deviceAges[0] > $this->deviceAges[2]) ? $this->deviceAges[0] : $this->deviceAges[2];
+            $highest  = ($this->deviceAges[4] > $highest) ? $this->deviceAges[4] : $highest;
+            $highest  = ($this->deviceAges[8] > $highest) ? $this->deviceAges[8] : $highest;
+            $barGraph = new gchart\gBarChart(375, 250);
+            $barGraph->setTitle("Age of printing devices");
+            $barGraph->setVisibleAxes(array('y'));
+
+            $colors = array("E21736", "A21746", "12AB36", "A2CB36");
+            foreach ($colors as $color)
+            {
+                $barGraph->addColors(array($color));
+            }
+            foreach (array_reverse($this->deviceAges) as $deviceCount)
+            {
+                $barGraph->addDataSet(array($deviceCount));
+            }
+            $barGraph->setLegend(array_reverse(Proposalgen_Model_Optimization_Abstract::$ageRanks));
+            $barGraph->setLegendPosition("b");
+            $barGraph->addAxisRange(0, 0, $highest * 1.1);
+            $barGraph->setDataRange(0, $highest * 1.1);
+            $barGraph->setBarScale(65, 10);
+            $barGraph->addValueMarkers($numberValueMarker, "000000", "0", "-1", "11");
+            $barGraph->addValueMarkers($numberValueMarker, "000000", "1", "-1", "11");
+            $barGraph->addValueMarkers($numberValueMarker, "000000", "2", "-1", "11");
+            $barGraph->addValueMarkers($numberValueMarker, "000000", "3", "-1", "11");
+            $this->_graphs [] = $barGraph->getUrl();
+            $this->_graphs [] = $proposalGraphs[3];
+            $this->_graphs [] = $proposalGraphs[7];
+            $this->_graphs [] = $proposalGraphs[8];
         }
+
         return $this->_graphs;
     }
 }
