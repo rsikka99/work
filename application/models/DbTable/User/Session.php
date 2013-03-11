d5ca38caf700f6a1a1c726315c6f53a961eca7fd
Shawn Wilder <swilder@tangentmtw.com>
2013-03-11 14:27:16 -0400

    MPSTOOLS-49 - Re-factored tables in models, mappers and db tables for user sessions so that session id is primary key.

--- application/models/DbTable/User/Session.php
+++ application/models/DbTable/User/Session.php
@@ -2,5 +2,5 @@
 class Application_Model_DbTable_User_Session extends Zend_Db_Table_Abstract
 {
     protected $_name = 'user_sessions';
-    protected $_primary = 'id';
+    protected $_primary = 'sessionId';
 }
