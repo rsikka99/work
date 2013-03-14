e4f0879e8eacb2e30ee5a6a226ac6bc51385855a
Shawn Wilder <swilder@tangentmtw.com>
2013-03-14 09:53:06 -0400

    MPSTOOLS-63 - Changed XML file to incorporate the new dealer report override.

--- application/modules/preferences/controllers/ProposalController.php
+++ application/modules/preferences/controllers/ProposalController.php
@@ -6,6 +6,85 @@ class Preferences_ProposalController extends Tangent_Controller_Action
      */
     public function indexAction ()
     {
+
+    }
+
+    /**
+     * This is where the admin can edit the system proposal preferences
+     */
+    public function systemAction ()
+    {
+        $this->view->title = 'Manage Settings';
+        $db                = Zend_Db_Table::getDefaultAdapter();
+
+        // Row #1 of Report Settings has all the system defaults
+        $systemReportSettings = Proposalgen_Model_Mapper_Report_Setting::getInstance()->find(1);
+        $systemSurveySettings = Proposalgen_Model_Mapper_Survey_Setting::getInstance()->find(1);
+        $form                 = new Proposalgen_Form_Settings_SystemAdmin();
+
+        if ($this->_request->isPost())
+        {
+            // Get the data that has been posted
+            $formData = $this->_request->getPost();
+
+            if ($form->isValid($formData))
+            {
+                if (isset($formData ['save_settings']))
+                {
+                    $db->beginTransaction();
+                    try
+                    {
+                        // Make all empty values = null
+                        foreach ($formData as &$value)
+                        {
+                            if (strlen($value) === 0)
+                            {
+                                $value = null;
+                            }
+                        }
+                        // Save page coverage settings (survey settings)
+                        $systemSurveySettings->populate($formData);
+                        Proposalgen_Model_Mapper_Survey_Setting::getInstance()->save($systemSurveySettings, $systemSurveySettings->id);
+                        // Save report settings (all other)
+                        $systemReportSettings->populate($formData);
+                        Proposalgen_Model_Mapper_Report_Setting::getInstance()->save($systemReportSettings, $systemReportSettings->id);
+
+                        $this->_helper->flashMessenger(array(
+                                                            "success" => "Your settings have been updated."
+                                                       ));
+                        $db->commit();
+                    }
+                    catch (Zend_Db_Exception $e)
+                    {
+                        $db->rollback();
+                        $this->_helper->flashMessenger(array("error" => "An error occured while saving your settings."));
+                    }
+                    catch (Exception $e)
+                    {
+                        $db->rollback();
+                        $this->_helper->flashMessenger(array("error" => "An error occured while saving your settings."));
+                    }
+                }
+            }
+            else
+            {
+                $this->_helper->flashMessenger(array(
+                                                    "error" => "Please review the errors below."
+                                               ));
+                $form->populate($formData);
+            }
+        }
+
+        // Add form to page
+        $form->setDecorators(array(array('ViewScript', array('viewScript' => 'forms/settings/systemadmin.phtml'))));
+        // Populate the form wif data
+        $form->populate($systemReportSettings->toArray());
+        $form->populate($systemSurveySettings->toArray());
+        $this->view->settingsForm = $form;
+    }
+
+    public function userAction ()
+    {
         $this->view->title = 'Manage My Settings';
         $db                = Zend_Db_Table::getDefaultAdapter();
         $message           = '';
@@ -114,77 +193,8 @@ class Preferences_ProposalController extends Tangent_Controller_Action
         $this->view->settingsForm = $form;
     }
 
-    /**
-     * This is where the admin can edit the system proposal preferences
-     */
-    public function systemAction ()
+    public function dealerAction ()
     {
-        $this->view->title = 'Manage Settings';
-        $db                = Zend_Db_Table::getDefaultAdapter();
 
-        // Row #1 of Report Settings has all the system defaults
-        $systemReportSettings = Proposalgen_Model_Mapper_Report_Setting::getInstance()->find(1);
-        $systemSurveySettings = Proposalgen_Model_Mapper_Survey_Setting::getInstance()->find(1);
-        $form                 = new Proposalgen_Form_Settings_SystemAdmin();
-
-        if ($this->_request->isPost())
-        {
-            // Get the data that has been posted
-            $formData = $this->_request->getPost();
-
-            if ($form->isValid($formData))
-            {
-                if (isset($formData ['save_settings']))
-                {
-                    $db->beginTransaction();
-                    try
-                    {
-                        // Make all empty values = null
-                        foreach ($formData as &$value)
-                        {
-                            if (strlen($value) === 0)
-                            {
-                                $value = null;
-                            }
-                        }
-                        // Save page coverage settings (survey settings)
-                        $systemSurveySettings->populate($formData);
-                        Proposalgen_Model_Mapper_Survey_Setting::getInstance()->save($systemSurveySettings, $systemSurveySettings->id);
-                        // Save report settings (all other)
-                        $systemReportSettings->populate($formData);
-                        Proposalgen_Model_Mapper_Report_Setting::getInstance()->save($systemReportSettings, $systemReportSettings->id);
-
-                        $this->_helper->flashMessenger(array(
-                                                            "success" => "Your settings have been updated."
-                                                       ));
-                        $db->commit();
-                    }
-                    catch (Zend_Db_Exception $e)
-                    {
-                        $db->rollback();
-                        $this->_helper->flashMessenger(array("error" => "An error occured while saving your settings."));
-                    }
-                    catch (Exception $e)
-                    {
-                        $db->rollback();
-                        $this->_helper->flashMessenger(array("error" => "An error occured while saving your settings."));
-                    }
-                }
-            }
-            else
-            {
-                $this->_helper->flashMessenger(array(
-                                                    "error" => "Please review the errors below."
-                                               ));
-                $form->populate($formData);
-            }
-        }
-
-        // Add form to page
-        $form->setDecorators(array(array('ViewScript', array('viewScript' => 'forms/settings/systemadmin.phtml'))));
-        // Populate the form wif data
-        $form->populate($systemReportSettings->toArray());
-        $form->populate($systemSurveySettings->toArray());
-        $this->view->settingsForm = $form;
     }
 }
