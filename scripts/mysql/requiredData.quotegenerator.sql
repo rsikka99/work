45247fb81332709e6df4a9c711e8e3d7127bac2d
Tyson Riehl <triehl@tangentmtw.com>
2013-03-21 08:09:08 -0400

    MPSTOOLS-65 - Changed default parts and labor cost

--- scripts/mysql/requiredData.quotegenerator.sql
+++ scripts/mysql/requiredData.quotegenerator.sql
@@ -1,6 +1,6 @@
 -- System Quote settings
 INSERT INTO `qgen_quote_settings` (`id`, `pageCoverageMonochrome`, `pageCoverageColor`, `deviceMargin`, `pageMargin`, `pricingConfigId`, `adminCostPerPage`, `laborCostPerPage`,`partsCostPerPage`) VALUES
-(1, 4.5, 20, 15, 20, 2, 0.0035, 0.0015,0.0006);
+(1, 4.5, 20, 15, 20, 2, 0.0035, 0.002,0.0015);
 
 INSERT INTO `qgen_dealer_quote_settings` (`dealerId`, `quoteSettingId`) VALUES
 (1,1);
