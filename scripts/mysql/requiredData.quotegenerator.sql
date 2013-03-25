87eeae1f667b75e13c551214b7439e04deef9296
skwilder <shawnkwilder@gmail.com>
2013-03-25 11:32:11 -0400

    MPSTOOLS-58 - Added default quote settings for dealers.

--- scripts/mysql/requiredData.quotegenerator.sql
+++ scripts/mysql/requiredData.quotegenerator.sql
@@ -1,9 +1,11 @@
 -- System Quote settings
 INSERT INTO `qgen_quote_settings` (`id`, `pageCoverageMonochrome`, `pageCoverageColor`, `deviceMargin`, `pageMargin`, `pricingConfigId`, `adminCostPerPage`, `laborCostPerPage`,`partsCostPerPage`) VALUES
-(1, 4.5, 20, 15, 20, 2, 0.0035, 0.002,0.0015);
+(1, 4.5, 20, 15, 20, 2, 0.0035, 0.002,0.0015),
+(2, 4.5, 20, 15, 20, 2, 0.0035, 0.002,0.0015);
 
 INSERT INTO `qgen_dealer_quote_settings` (`dealerId`, `quoteSettingId`) VALUES
-(1,1);
+(1,1),
+(2,2);
 
 -- ---------------------------------
 -- Create a default leasing schema
