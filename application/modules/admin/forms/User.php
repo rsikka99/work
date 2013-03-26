2d5b817086c8aa18afdd33767ab2e4ea97a85e85
Tyson Riehl <triehl@tangentmtw.com>
2013-03-26 10:07:35 -0400

    MPSTOOLS-69 - fixed a spelling error

--- application/modules/admin/forms/User.php
+++ application/modules/admin/forms/User.php
@@ -157,7 +157,7 @@ class Admin_Form_User extends EasyBib_Form
             }
             $this->addElement($userRoles);
         }
-        $isAdmin = $this->getView()->IsAllowed(Application_Model_Acl::RESOURCE_ADMIN_USER_WILDCARD_WILDCARD, Application_Model_Acl::PRIVILEGE_ADMIN);
+        $isAdmin = $this->getView()->IsAllowed(Application_Model_Acl::RESOURCE_ADMIN_USER_WILDCARD, Application_Model_Acl::PRIVILEGE_ADMIN);
         if ($isAdmin && $this->dealerManagement == false)
         {
             $firstDealerId = null;
