e3dead2997a6ecf87509d1cc9a6039a4627305b2
lrobert <lrobert@tangentmtw.com>
2013-04-11 13:26:10 -0400

    Fixed an issue where logos wouldn't fall back to the MPS toolbox logo if not present.

--- application/modules/proposalgen/library/Controller/Proposal.php
+++ application/modules/proposalgen/library/Controller/Proposal.php
@@ -205,19 +205,19 @@ class Proposalgen_Library_Controller_Proposal extends Tangent_Controller_Action
 
 
         // Setup the different file formats
-        $this->_csvFormat   = (object)array(
+        $this->_csvFormat           = (object)array(
             'extension'      => 'csv',
             'name'           => 'Excel (CSV)',
             'loadingmessage' => '',
             'btnstyle'       => 'success'
         );
-        $this->_pdfFormat   = (object)array(
+        $this->_pdfFormat           = (object)array(
             'extension'      => 'pdf',
             'name'           => 'Adobe PDF',
             'loadingmessage' => 'Please be patient as PDF\'s take a few minutes to generate',
             'btnstyle'       => 'danger'
         );
-        $this->_wordFormat  = (object)array(
+        $this->_wordFormat          = (object)array(
             'extension'      => 'docx',
             'name'           => 'Word (DOCX)',
             'loadingmessage' => 'Please wait a moment while we generate your report',
@@ -244,11 +244,13 @@ class Proposalgen_Library_Controller_Proposal extends Tangent_Controller_Action
 
     /**
      * Gets the dealer logo file relative to the public folder
+     *
      * @return string
      */
     public function getDealerLogoFile ()
     {
-        $dealer = Admin_Model_Mapper_Dealer::getInstance()->find($this->_dealerId);
+        $dealer   = Admin_Model_Mapper_Dealer::getInstance()->find($this->_dealerId);
+        $logoFile = false;
         if ($dealer)
         {
             if ($dealer->dealerLogoImageId > 0)
@@ -256,7 +258,9 @@ class Proposalgen_Library_Controller_Proposal extends Tangent_Controller_Action
                 $logoFile = $dealer->getDealerLogoImageFile();
             }
         }
-        else
+
+
+        if ($logoFile == false)
         {
             $logoFile = $this->view->theme("proposalgenerator/reports/images/mpstoolbox_logo.jpg");
         }
