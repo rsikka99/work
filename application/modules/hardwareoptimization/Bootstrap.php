9d018ef04c52e4fc26ce6a233aeba5e49de90aec
lrobert <lrobert@tangentmtw.com>
2013-04-16 14:52:33 -0400

    MPSTOOLS-96 - Added view models to the hardware optimization bootstrap

--- application/modules/hardwareoptimization/Bootstrap.php
+++ application/modules/hardwareoptimization/Bootstrap.php
@@ -3,14 +3,12 @@ class Hardwareoptimization_Bootstrap extends Zend_Application_Module_Bootstrap
 {
     protected function _initNavigation ()
     {
+        $config = new Zend_Config_Xml(__DIR__ . '/configs/navigation.xml', 'nav');
         /* @var $container Zend_Navigation */
-
-        $view      = $this->getApplication()->getResource('view');
-
-        $config    = new Zend_Config_Xml(__DIR__ . '/configs/navigation.xml', 'nav');
         $container = Zend_Registry::get('Zend_Navigation');
         $container->addPages($config);
     }
+
     /**
      * Sets the paginator view partials
      */
@@ -22,6 +20,11 @@ class Hardwareoptimization_Bootstrap extends Zend_Application_Module_Bootstrap
     protected function _initLibraryAutoloader ()
     {
         return $this->getResourceLoader()->addResourceType('library', 'library', 'library');
+    }
+
+    protected function _initViewModelAutoloader ()
+    {
+        $this->getResourceLoader()->addResourceType('ViewModel', 'viewmodels', 'ViewModel');
     }
 
 }
