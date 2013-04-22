17dcb4346ddab8196689e4981ec214fc265c1352
lrobert <lrobert@tangentmtw.com>
2013-04-22 10:00:46 -0400

    MPSTOOLS-96 - Added a navigation view helper to the my library

---
+++ library/My/View/Helper/NavigationMenu.php
@@ -0,0 +1,61 @@
+<?php
+class My_View_Helper_NavigationMenu extends Zend_View_Helper_Abstract
+{
+
+    /**
+     * @param My_Navigation_Step[] $steps
+     *
+     * @return string
+     */
+    public function NavigationMenu ($steps = null)
+    {
+        $html = array();
+        if ($steps !== null)
+        {
+            $html [] = '<ul class="nav nav-tabs" id="assessmentMenu">';
+
+            foreach ($steps as $step)
+            {
+                if ($step->active)
+                {
+                    $html [] = "<li class='active'>";
+                }
+                else if ($step->canAccess)
+                {
+                    $html [] = "<li>";
+                }
+                else
+                {
+                    $html [] = "<li class='disabled'>";
+                }
+
+                // Get the url and name
+                $url  = $this->view->url(array(
+                                              'controller' => $step->controller,
+                                              'action'     => $step->action
+                                         ));
+                $name = $step->name;
+
+                if ($step->canAccess)
+                {
+                    $html [] = "<a href='{$url}'>{$name}</a>";
+                }
+                else
+                {
+                    $html [] = "<p>{$name}</p>";
+                }
+
+
+                $html [] = "<li>";
+
+                if ($step->nextStep !== null && !$step->canAccess)
+                {
+//                    break;
+                }
+            }
+            $html [] = '</ul>';
+        }
+
+        return implode(PHP_EOL, $html);
+    }
+}
