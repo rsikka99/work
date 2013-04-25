f7c9efa1c2a142e34134f58e5ceb44c5e73f8a9f
skwilder <shawnkwilder@gmail.com>
2013-04-25 15:41:50 -0400

    MPSTOOLS-96 - Dealer and customer report controllers for hardware optmization finished off.

--- application/modules/hardwareoptimization/controllers/Report/Customer/OptimizationController.php
+++ application/modules/hardwareoptimization/controllers/Report/Customer/OptimizationController.php
@@ -1,7 +1,14 @@
 <?php
 class Hardwareoptimization_Report_Customer_OptimizationController extends Hardwareoptimization_Library_Controller_Action
 {
-    public function indexAction(){
+    public function indexAction()
+    {
+        $this->_navigation->setActiveStep(Hardwareoptimization_Model_Hardware_Optimization_Steps::STEP_FINISHED);
+        $this->initReportList();
+        $this->view->headScript()->prependFile($this->view->baseUrl("/js/htmlReport.js"));
 
+        $this->view->availableReports['CustomerOptimization']['active'] = true;
+        $this->view->hardwareOptimization = $this->_hardwareOptimization;
+        $this->view->optimization          = $this->getOptimizationViewModel();
     }
 }
--- application/modules/hardwareoptimization/controllers/Report/Dealer/OptimizationController.php
+++ application/modules/hardwareoptimization/controllers/Report/Dealer/OptimizationController.php
@@ -4,10 +4,14 @@ class Hardwareoptimization_Report_Dealer_OptimizationController extends Hardware
     public function indexAction ()
     {
         $this->_navigation->setActiveStep(Hardwareoptimization_Model_Hardware_Optimization_Steps::STEP_FINISHED);
-        $this->initReportList();
-        $this->view->headScript()->prependFile($this->view->baseUrl("/js/htmlReport.js"));
 
-        $this->view->hardwareOptimization = $this->_hardwareOptimization;
-        $this->view->optimization          = $this->getOptimizationViewModel();
+        $this->initHtmlReport();
+        $this->initReportList();
+        $this->view->availableReports['DealerOptimization']['active'] = true;
+
+//        $this->view->headScript()->prependFile($this->view->baseUrl("/js/htmlReport.js"));
+
+        $this->view->hardwareOptimization                                     = $this->_hardwareOptimization;
+        $this->view->optimization                                             = $this->getOptimizationViewModel();
     }
 }
