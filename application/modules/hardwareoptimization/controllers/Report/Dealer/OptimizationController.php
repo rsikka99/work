7cb55ff9e860a250a99d09bb38aed432987b4157
skwilder <shawnkwilder@gmail.com>
2013-04-25 09:41:27 -0400

    MPSTOOLS-96 - Added the report list to the index controller for dealer optimization controller

--- application/modules/hardwareoptimization/controllers/Report/Dealer/OptimizationController.php
+++ application/modules/hardwareoptimization/controllers/Report/Dealer/OptimizationController.php
@@ -3,6 +3,9 @@ class Hardwareoptimization_Report_Dealer_OptimizationController extends Hardware
 {
     public function indexAction ()
     {
+        $this->_navigation->setActiveStep(Hardwareoptimization_Model_Hardware_Optimization_Steps::STEP_FINISHED);
+        $this->initReportList();
+        $this->view->headScript()->prependFile($this->view->baseUrl("/js/htmlReport.js"));
 
         $this->view->hardwareOptimization = $this->_hardwareOptimization;
         $this->view->optimization          = $this->getOptimizationViewModel();
