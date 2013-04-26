945a59562d958853d6250c7bfe1480efd763c130
skwilder <shawnkwilder@gmail.com>
2013-04-26 10:05:57 -0400

    MPSTOOLS-96 - Added the logic inside Dealer_Optimization_Controller to generate the word document for dealer hardware optimization

--- application/modules/hardwareoptimization/controllers/Report/Dealer/OptimizationController.php
+++ application/modules/hardwareoptimization/controllers/Report/Dealer/OptimizationController.php
@@ -7,11 +7,69 @@ class Hardwareoptimization_Report_Dealer_OptimizationController extends Hardware
 
         $this->initHtmlReport();
         $this->initReportList();
+
+
+        $this->view->formats = array(
+            "/hardwareoptimization/report_dealer_optimization/generate/format/docx" => $this->_wordFormat
+        );
+
+        try
+        {
+            $this->clearCacheForReport();
+            $this->view->optimization = $this->getOptimizationViewModel();
+        }
+        catch (Exception $e)
+        {
+            throw new Exception("Couldn't generate hardware optimization.", 0, $e);
+        }
+
+
         $this->view->availableReports['DealerOptimization']['active'] = true;
+        $this->view->hardwareOptimization                             = $this->_hardwareOptimization;
+    }
 
-//        $this->view->headScript()->prependFile($this->view->baseUrl("/js/htmlReport.js"));
+    /**
+     * The Index action of the solution.
+     */
+    public function generateAction ()
+    {
+        $format = $this->_getParam("format", "docx");
 
-        $this->view->hardwareOptimization                                     = $this->_hardwareOptimization;
-        $this->view->optimization                                             = $this->getOptimizationViewModel();
+        switch ($format)
+        {
+            case "csv" :
+                throw new Exception("CSV Format not available through this page yet!");
+                break;
+            case "docx" :
+                $dealerOptimization = new Hardwareoptimization_Model_Optimization_Dealer($this->_hardwareOptimization);
+                $graphs             = $this->cachePNGImages($dealerOptimization->getGraphs(), true);
+
+                require_once ('PHPWord.php');
+                $this->view->phpword    = new PHPWord();
+                $this->view->wordStyles = $this->getWordStyles();
+                $this->view->graphs     = $graphs;
+
+                $this->_helper->layout->disableLayout();
+                break;
+            case "pdf" :
+                throw new Exception("PDF Format not available through this page yet!");
+                break;
+            default :
+                throw new Exception("Invalid Format Requested!");
+                break;
+        }
+
+        $filename = "dealerHardwareOptimization.$format";
+
+        $this->initReportVariables($filename);
+        // Render early
+        try
+        {
+            $this->render($format . "/00_render");
+        }
+        catch (Exception $e)
+        {
+            throw new Exception("Controller caught the exception!", 0, $e);
+        }
     }
 }
