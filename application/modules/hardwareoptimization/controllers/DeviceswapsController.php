917bed989f131b3600dea8671016a15a387495aa
skwilder <shawnkwilder@gmail.com>
2013-05-03 11:13:18 -0400

    MPSTOOLS-99 - Added the device swaps controller

--- application/modules/hardwareoptimization/controllers/DeviceswapsController.php
+++ application/modules/hardwareoptimization/controllers/DeviceswapsController.php
@@ -4,9 +4,54 @@
  */
 class Hardwareoptimization_DeviceswapsController extends Tangent_Controller_Action
 {
+    /**
+     * The Zend_Auth identity
+     *
+     * @var stdClass
+     */
+    protected $_identity;
+
+    public function init ()
+    {
+        $this->_identity = Zend_Auth::getInstance()->getIdentity();
+    }
+
     public function indexAction ()
     {
-        $form             = new Hardwareoptimization_Form_DeviceSwaps();
-        $this->view->form = $form;
+        $form = new Hardwareoptimization_Form_DeviceSwaps();
+
+        if ($this->_request->isPost())
+        {
+            $postData = $this->_request->getPost();
+            if (!isset($value['goBack']))
+            {
+                if ($form->isValid($postData))
+                {
+                    $formData = $form->getValues();
+
+                    // save the device swap to the database
+                    $formData['dealerId'] = $this->_identity->dealerId;
+                    $deviceSwap           = new Hardwareoptimization_Model_Device_Swap();
+                    if ($deviceSwap->saveObject($formData))
+                    {
+                        $this->_flashMessenger->addMessage(array("success" => "Device swap successfully added."));
+                        // If save and continue re-direct to settings page
+                        if (isset($formData['saveAndContinue']))
+                        {
+                            $this->redirector("index", "admin", "default");
+                        }
+                    }
+                    else
+                    {
+                        $this->_flashMessenger->addMessage(array("danger" => "Adding device swap failed. Please try again."));
+                    }
+                }
+
+            }
+        }
+
+        $this->view->deviceSwap     = new Hardwareoptimization_ViewModel_DeviceSwap();
+        $this->view->navigationForm = new Hardwareoptimization_Form_Hardware_Optimization_Navigation();
+        $this->view->form           = $form;
     }
 }
