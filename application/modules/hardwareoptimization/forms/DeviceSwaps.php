c9ac2ae10f875ccdcd297ce8273a68724b0c2b5c
skwilder <shawnkwilder@gmail.com>
2013-05-03 11:12:52 -0400

    MPSTOOLS-99 - Added the form for device swaps

--- application/modules/hardwareoptimization/forms/DeviceSwaps.php
+++ application/modules/hardwareoptimization/forms/DeviceSwaps.php
@@ -6,11 +6,11 @@ class Hardwareoptimization_Form_DeviceSwaps extends Twitter_Bootstrap_Form_Horiz
         $this->setMethod("POST");
         $this->_addClassNames('reportSettingsForm form-center-actions');
 
-        $this->addElement("text", "deviceName", array("label" => "Device Name"));
+        $this->addElement("text", "masterDeviceId", array("label" => "Device Name", "class" => "input-xlarge", "description" => 'Only "can sell" devices can be used as replacements.'));
 
-        $this->addElement("text", "maximumPageCountThreshold", array("label" => "Max Page Count Volume"));
-        $this->addElement("text", "minimumPageCountThreshold", array("label" => "Min Page Count Volume"));
+        $this->addElement("text", "minimumPageCount", array("label" => "Max Page Count Volume"));
+        $this->addElement("text", "maximumPageCount", array("label" => "Min Page Count Volume"));
 
-        Hardwareoptimization_Form_Hardware_Optimization_Navigation::addFormActionsToForm(Hardwareoptimization_Form_Hardware_Optimization_Navigation::BUTTONS_ALL, $this);
+        Hardwareoptimization_Form_Hardware_Optimization_Navigation::addFormActionsToForm(Hardwareoptimization_Form_Hardware_Optimization_Navigation::BUTTONS_SAVE, $this);
     }
 }
