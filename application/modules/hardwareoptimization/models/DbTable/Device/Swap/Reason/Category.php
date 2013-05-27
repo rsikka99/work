034ad26f425c1814225b2ff149419eb40cbdbe1f
skwilder <shawnkwilder@gmail.com>
2013-05-27 13:58:13 -0400

    Added model, mapper, and DbTable for a device swap reason category:

---
+++ application/modules/hardwareoptimization/models/DbTable/Device/Swap/Reason/Category.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Class Hardwareoptimization_Model_DbTable_Device_Swap_Reason_Category
+ */
+class Hardwareoptimization_Model_DbTable_Device_Swap_Reason_Category extends Zend_Db_Table_Abstract
+{
+    protected $_name = "device_swap_reason_categories";
+    protected $_primary = "id";
+}
