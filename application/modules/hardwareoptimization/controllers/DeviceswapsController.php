db64f2ec858bc5fe5e17b1193b7d5cb443e3ada6
skwilder <shawnkwilder@gmail.com>
2013-05-29 07:36:23 -0400

    Added requried functions in the device swap controller to handle jquery request for device swap reaosns

--- application/modules/hardwareoptimization/controllers/DeviceswapsController.php
+++ application/modules/hardwareoptimization/controllers/DeviceswapsController.php
@@ -18,41 +18,14 @@ class Hardwareoptimization_DeviceswapsController extends Tangent_Controller_Acti
 
     public function indexAction ()
     {
-        $form = new Hardwareoptimization_Form_DeviceSwaps();
-
-        if ($this->_request->isPost())
-        {
-            $postData = $this->_request->getPost();
-            if (!isset($value['goBack']))
-            {
-                if ($form->isValid($postData))
-                {
-                    $formData = $form->getValues();
-
-                    // save the device swap to the database
-                    $formData['dealerId'] = $this->_identity->dealerId;
-                    $deviceSwap           = new Hardwareoptimization_Model_Device_Swap();
-                    if ($deviceSwap->saveObject($formData))
-                    {
-                        $this->_flashMessenger->addMessage(array("success" => "Device swap successfully added."));
-                        // If save and continue re-direct to settings page
-                        if (isset($formData['saveAndContinue']))
-                        {
-                            $this->redirector("index", "admin", "default");
-                        }
-                    }
-                    else
-                    {
-                        $this->_flashMessenger->addMessage(array("danger" => "Adding device swap failed. Please try again."));
-                    }
-                }
-
-            }
-        }
+        $form           = new Hardwareoptimization_Form_DeviceSwaps();
+        $deviceSwapForm = new Hardwareoptimization_Form_DeviceSwapReasons();
 
         $this->view->deviceSwap     = new Hardwareoptimization_ViewModel_DeviceSwap();
+        $this->view->reasons        = Hardwareoptimization_Model_Mapper_Device_Swap_Reason::getInstance()->fetchAllReasonByDealerId($this->_identity->dealerId);
         $this->view->navigationForm = new Hardwareoptimization_Form_Hardware_Optimization_Navigation();
         $this->view->form           = $form;
+        $this->view->deviceSwapForm = $deviceSwapForm;
     }
 
     public function deviceSwapListAction ()
@@ -138,6 +111,82 @@ class Hardwareoptimization_DeviceswapsController extends Tangent_Controller_Acti
         $this->sendJson($json);
     }
 
+    public function deviceReasonListAction ()
+    {
+        $jsonArray              = array();
+        $jqGridService          = new Tangent_Service_JQGrid();
+        $deviceSwapReasonMapper = Hardwareoptimization_Model_Mapper_Device_Swap_Reason::getInstance();
+
+        /*
+         * Grab the incoming parameters
+         */
+        $jqGridServiceParameters = array(
+            'sidx' => $this->_getParam('sidx', 'reason'),
+            'sord' => $this->_getParam('sord', 'desc'),
+            'page' => $this->_getParam('page', 1),
+            'rows' => $this->_getParam('rows', 10)
+        );
+
+        // Set up validation arrays
+        $blankModel  = new Hardwareoptimization_Model_Device_Swap_Reason();
+        $sortColumns = array_keys($blankModel->toArray());
+
+        $jqGridService->parseJQGridPagingRequest($jqGridServiceParameters);
+        $jqGridService->setValidSortColumns($sortColumns);
+
+
+        if ($jqGridService->sortingIsValid())
+        {
+            $jqGridService->setRecordCount($deviceSwapReasonMapper->fetAllForDealer($this->_identity->dealerId, null, null, null, true));
+
+            // Validate current page number since we don't want to be out of bounds
+            if ($jqGridService->getCurrentPage() < 1)
+            {
+                $jqGridService->setCurrentPage(1);
+            }
+            else if ($jqGridService->getCurrentPage() > $jqGridService->calculateTotalPages())
+            {
+                $jqGridService->setCurrentPage($jqGridService->calculateTotalPages());
+            }
+
+            // Return a small subset of the results based on the jqGrid parameters
+            $startRecord = $jqGridService->getRecordsPerPage() * ($jqGridService->getCurrentPage() - 1);
+
+            if ($startRecord < 0)
+            {
+                $startRecord = 0;
+            }
+
+            $sortOrder = array();
+
+            if ($jqGridService->hasGrouping())
+            {
+                $sortOrder[] = $jqGridService->getGroupByColumn() . ' ' . $jqGridService->getGroupBySortOrder();
+            }
+
+            if ($jqGridService->hasColumns())
+            {
+                $sortOrder[] = $jqGridService->getSortColumn() . ' ' . $jqGridService->getSortDirection();
+            }
+
+
+            $jqGridService->setRows($deviceSwapReasonMapper->fetAllForDealer($this->_identity->dealerId, $sortOrder, $jqGridService->getRecordsPerPage(), $startRecord));
+
+            // Send back jqGrid json data
+            $this->sendJson($jqGridService->createPagerResponseArray());
+        }
+        else
+        {
+            $this->_response->setHttpResponseCode(500);
+            $this->sendJson(array(
+                                 'error' => 'Sorting parameters are invalid'
+                            ));
+        }
+
+        $json = json_encode($jsonArray);
+        $this->sendJson($json);
+    }
+
     public function updateDeviceAction ()
     {
         $postData = array(
@@ -170,6 +219,45 @@ class Hardwareoptimization_DeviceswapsController extends Tangent_Controller_Acti
             $this->sendJson($deviceSwap);
         }
 
+    }
+
+    public function updateDeviceReasonAction ()
+    {
+        $postData = array(
+            "reasonCategory" => $this->getParam("reasonCategory"),
+            "reason"         => $this->getParam("reason"),
+            "id"             => $this->getParam('deviceSwapReasonId')
+        );
+
+        // Does the reason exist
+        $reasonCategory = Hardwareoptimization_Model_Mapper_Device_Swap_Reason_Category::getInstance()->find($postData['reasonCategory']);
+
+        if ($reasonCategory instanceof Hardwareoptimization_Model_Device_Swap_Reason_Category)
+        {
+            // Is this an edit reason ?
+            $reason = Hardwareoptimization_Model_Mapper_Device_Swap_Reason::getInstance()->find($postData['id']);
+
+            if ($reason instanceof Hardwareoptimization_Model_Device_Swap_Reason)
+            {
+                $reason->deviceSwapReasonCategoryId = $reasonCategory->id;
+                $reason->reason                     = $postData['reason'];
+                Hardwareoptimization_Model_Mapper_Device_Swap_Reason::getInstance()->save($reason);
+            }
+            else
+            {
+                $reason                             = new Hardwareoptimization_Model_Device_Swap_Reason();
+                $reason->dealerId                   = $this->_identity->dealerId;
+                $reason->deviceSwapReasonCategoryId = $reasonCategory->id;
+                $reason->reason                     = $postData['reason'];
+                Hardwareoptimization_Model_Mapper_Device_Swap_Reason::getInstance()->insert($reason);
+            }
+        }
+        else
+        {
+            $this->sendJsonError("Cannot find reason category, try again.");
+        }
+
+        $this->sendJson(array());
     }
 
     public function deleteDeviceAction ()
