db37d9283f446495b9bc3f8dd05d8f4a9e2d3cc9
skwilder <shawnkwilder@gmail.com>
2013-06-05 07:52:13 -0400

    Removed dealer margin from the sql script.

--- scripts/mysql/schema.sql
+++ scripts/mysql/schema.sql
@@ -293,6 +293,7 @@ CREATE  TABLE IF NOT EXISTS `device_instances` (
     `pageCoverageCyan` DOUBLE NULL DEFAULT NULL ,
     `pageCoverageMagenta` DOUBLE NULL DEFAULT NULL ,
     `pageCoverageYellow` DOUBLE NULL DEFAULT NULL ,
+    `deviceSwapReasonId` INT(11) NULL ,
     PRIMARY KEY (`id`) ,
     INDEX `device_instances_ibfk_2_idx` (`rmsUploadRowId` ASC) ,
     INDEX `device_instances_ibfk_1_idx` (`rmsUploadId` ASC) ,
@@ -1342,7 +1343,6 @@ CREATE  TABLE IF NOT EXISTS `hardware_optimization_settings` (
     `id` INT NOT NULL AUTO_INCREMENT ,
     `costThreshold` DOUBLE NULL ,
     `customerPricingConfigId` INT NULL ,
-    `dealerMargin` DOUBLE NULL ,
     `dealerPricingConfigId` INT NULL ,
     `targetColorCostPerPage` DOUBLE NULL ,
     `targetMonochromeCostPerPage` DOUBLE NULL ,
@@ -1810,6 +1810,93 @@ CREATE  TABLE IF NOT EXISTS `privileges` (
     CONSTRAINT `privileges_ibfk_1`
     FOREIGN KEY (`roleId` )
     REFERENCES `roles` (`id` )
+        ON DELETE CASCADE
+        ON UPDATE CASCADE);
+
+
+-- -----------------------------------------------------
+-- Table `device_swap_reason_categories`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `device_swap_reason_categories` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT ,
+    `name` VARCHAR(255) NOT NULL ,
+    PRIMARY KEY (`id`) );
+
+
+-- -----------------------------------------------------
+-- Table `device_swap_reasons`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `device_swap_reasons` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT ,
+    `dealerId` INT(11) NOT NULL ,
+    `deviceSwapReasonCategoryId` INT(11) NOT NULL ,
+    `reason` VARCHAR(255) NOT NULL ,
+    PRIMARY KEY (`id`) ,
+    INDEX `dealer_swap_reasons_ibfk1_idx` (`dealerId` ASC) ,
+    INDEX `dealer_swap_reasons_ibfk2_idx` (`deviceSwapReasonCategoryId` ASC) ,
+    CONSTRAINT `dealer_swap_reasons_ibfk1`
+    FOREIGN KEY (`dealerId` )
+    REFERENCES `dealers` (`id` )
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
+    CONSTRAINT `dealer_swap_reasons_ibfk2`
+    FOREIGN KEY (`deviceSwapReasonCategoryId` )
+    REFERENCES `device_swap_reason_categories` (`id` )
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION);
+
+
+-- -----------------------------------------------------
+-- Table `device_swap_reason_defaults`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `device_swap_reason_defaults` (
+    `deviceSwapReasonCategoryId` INT(11) NOT NULL ,
+    `dealerId` INT NOT NULL ,
+    `deviceSwapReasonId` INT(11) NOT NULL ,
+    PRIMARY KEY (`deviceSwapReasonCategoryId`, `dealerId`) ,
+    INDEX `device_swap_defaults_ibk2_idx` (`deviceSwapReasonId` ASC) ,
+    INDEX `device_swap_reason_category_defaults_ibkf1_idx` (`dealerId` ASC) ,
+    CONSTRAINT `device_swap_reason_defaults_ibfk2`
+    FOREIGN KEY (`deviceSwapReasonId` )
+    REFERENCES `device_swap_reasons` (`id` )
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
+    CONSTRAINT `device_swap_reason_defaults_ibkf1`
+    FOREIGN KEY (`deviceSwapReasonCategoryId` )
+    REFERENCES `device_swap_reason_categories` (`id` )
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
+    CONSTRAINT `device_swap_reason_defaults_ibkf3`
+    FOREIGN KEY (`dealerId` )
+    REFERENCES `dealers` (`id` )
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION);
+
+
+-- -----------------------------------------------------
+-- Table `device_instance_device_swap_reasons`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `device_instance_device_swap_reasons` (
+    `hardwareOptimizationId` INT(11) NOT NULL ,
+    `deviceInstanceId` INT(11) NOT NULL ,
+    `deviceSwapReasonId` INT(11) NOT NULL ,
+    PRIMARY KEY (`hardwareOptimizationId`, `deviceInstanceId`) ,
+    INDEX `device_instance_device_swap_reasons_ibkf1_idx` (`hardwareOptimizationId` ASC) ,
+    INDEX `device_instance_device_swap_reasons_ibkf2_idx` (`deviceInstanceId` ASC) ,
+    INDEX `device_instance_device_swap_reasons_ibkf3_idx` (`deviceSwapReasonId` ASC) ,
+    CONSTRAINT `device_instance_device_swap_reasons_ibkf1`
+    FOREIGN KEY (`hardwareOptimizationId` )
+    REFERENCES `hardware_optimizations` (`id` )
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
+    CONSTRAINT `device_instance_device_swap_reasons_ibkf2`
+    FOREIGN KEY (`deviceInstanceId` )
+    REFERENCES `device_instances` (`id` )
+        ON DELETE NO ACTION
+        ON UPDATE NO ACTION,
+    CONSTRAINT `device_instance_device_swap_reasons_ibkf3`
+    FOREIGN KEY (`deviceSwapReasonId` )
+    REFERENCES `device_swap_reasons` (`id` )
         ON DELETE CASCADE
         ON UPDATE CASCADE);
 
