6c23478dbd17779a16233c5fabd8427da7418b8d
Tyson Riehl <triehl@tangentmtw.com>
2013-08-06 10:24:52 -0400

    Close GH-65: Added the updateTonerVendorByManufacturerId procedure back into our system..

---
+++ migrations/20130801075842_add_update_toner_vendor_by_manufacturer_id_procedure.php
@@ -0,0 +1,78 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class AddUpdateTonerVendorByManufacturerIdProcedure extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     *
+     * Uncomment this method if you would like to use it.
+     *
+    public function change()
+    {
+    }
+     */
+
+    /**
+     * Migrate Up.
+     */
+    public function up ()
+    {
+        $this->execute('CREATE PROCEDURE updateTonerVendorByManufacturerId(IN inManufacturerId INT(11))
+        /*
+         * Updates toner vendor status. It will mark new toner vendors and delete ones that no longer have toners assigned to devices.
+         */
+            BEGIN
+                DECLARE shouldBeTonerVendor INT DEFAULT 0;
+                DECLARE isCurrentlyTonerVendor INT DEFAULT 0;
+
+                # New Status
+                SELECT
+                    IF(COUNT(*) > 0, 1, 0)
+                INTO shouldBeTonerVendor
+                FROM toners AS t
+                    JOIN device_toners AS dt
+                        ON dt.toner_id = t.id
+                    JOIN manufacturers AS m
+                        ON m.id = t.manufacturerId
+                    JOIN master_devices AS md
+                        ON dt.master_device_id = md.id
+                    JOIN manufacturers AS mdm
+                        ON mdm.id = md.manufacturerId
+                WHERE t.manufacturerId != md.manufacturerId AND t.manufacturerId = inManufacturerId;
+
+
+                # Current Status
+                SELECT
+                    IF(COUNT(*) > 0, 1, 0)
+                INTO isCurrentlyTonerVendor
+                FROM toner_vendor_manufacturers AS tvm
+                WHERE tvm.manufacturerId = inManufacturerId;
+
+                # Update Status
+                IF shouldBeTonerVendor = 0 AND isCurrentlyTonerVendor = 1
+                THEN
+                    DELETE FROM toner_vendor_manufacturers
+                    WHERE manufacturerId = inManufacturerId;
+                ELSE
+                    IF shouldBeTonerVendor = 1 AND isCurrentlyTonerVendor = 0
+                    THEN
+                        INSERT INTO toner_vendor_manufacturers VALUES (inManufacturerId);
+                    END IF;
+                END IF;
+            END'
+        );
+    }
+
+    /**
+     * Migrate Down.
+     */
+    public function down ()
+    {
+        $this->execute('DROP PROCEDURE updateTonerVendorByManufacturerId');
+    }
+}
