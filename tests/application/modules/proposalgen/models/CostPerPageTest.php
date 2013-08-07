287b71265ab3987adaf05ab445ad9797908d52d9
lrobert <lrobert@tangentmtw.com>
2013-08-07 15:03:18 -0400

    Added cost per page model test

---
+++ tests/application/modules/proposalgen/models/CostPerPageTest.php
@@ -0,0 +1,44 @@
+<?php
+
+class Proposalgen_Model_CostPerPageTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Proposalgen_Model_CostPerPage
+     */
+    protected $_costPerPage;
+
+    /**
+     * @param bool $forceNewObject
+     *
+     * @return Proposalgen_Model_CostPerPage
+     */
+    public function getCostPerPage ($forceNewObject = false)
+    {
+        if (!isset($this->_costPerPage) || $forceNewObject)
+        {
+            $this->_costPerPage = new Proposalgen_Model_CostPerPage();
+        }
+
+        return $this->_costPerPage;
+    }
+
+    public function testCostPerPageIsZero ()
+    {
+        $costPerPage = $this->getCostPerPage();
+        $this->assertTrue($costPerPage->monochromeCostPerPage === 0 && $costPerPage->colorCostPerPage === 0);
+
+        $this->assertSame($costPerPage->toArray(), array('monochromeCostPerPage' => 0, 'colorCostPerPage' => 0));
+    }
+
+    public function testAddingCostPerPage ()
+    {
+        $costPerPage                        = $this->getCostPerPage();
+        $costPerPage->monochromeCostPerPage = 0.05;
+        $costPerPage->colorCostPerPage      = 0.95;
+
+        $costPerPage->add($costPerPage);
+
+        $this->assertTrue($costPerPage->monochromeCostPerPage === 0.10 && $costPerPage->colorCostPerPage === 1.9);
+        $this->assertSame($costPerPage->toArray(), array('monochromeCostPerPage' => 0.10, 'colorCostPerPage' => 1.9));
+    }
+}
