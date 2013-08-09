2e57f951ac0449b79ea74da09b91621f5fa5a5f5
Myke Pwnage <mpwnage@tangentmtw.com>
2013-08-09 15:38:03 -0400

    Close GH-86: Mpstools 190 unit test ho form setting test.

---
+++ tests/application/modules/hardwareoptimization/forms/SettingTest.php
@@ -0,0 +1,99 @@
+<?php
+
+/**
+ * Class Hardwareoptimization_Form_SettingTest
+ */
+class Hardwareoptimization_Form_SettingTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Hardwareoptimization_Form_Setting
+     */
+    protected $_form;
+
+    public function setUp ()
+    {
+        $this->_form = new Hardwareoptimization_Form_Setting();
+        parent::setUp();
+    }
+
+    public function tearDown ()
+    {
+        parent::tearDown();
+        $this->_form = null;
+    }
+
+    /**
+     * This function loads an XML file of good data into arrays to be tested in the form
+     */
+    public function goodHOSettingData ()
+    {
+        $xml = simplexml_load_file(__DIR__ . "/_files/goodData_hardwareFormTest.xml");
+        $settings = array();
+        foreach ($xml->setting as $setting)
+        {
+            $settings[] = (array)$setting;
+        }
+
+        return $settings;
+    }
+
+    /**
+     * This function loads an XML file of bad data into arrays to be tested in the form
+     */
+    public function badHOSettingData ()
+    {
+        $xml = simplexml_load_file(__DIR__ . "/_files/badData_hardwareFormTest.xml");
+        $settings = array();
+        foreach ($xml->setting as $setting)
+        {
+            $settings[] = (array)$setting;
+        }
+
+        return $settings;
+    }
+
+    /**
+     * Test the form using valid data
+     *
+     *
+     * @dataProvider goodHOSettingData
+     */
+    public function testFormAcceptsValidData ($name, $costThreshold, $pageCoverageMonochrome, $pageCoverageColor, $adminCostPerPage, $partsCostPerPage, $laborCostPerPage, $targetMonochromeCostPerPage, $targetColorCostPerPage)
+    {
+        $data = array(
+            'name'                        => $name,
+            'costThreshold'               => $costThreshold,
+            'pageCoverageMonochrome'      => $pageCoverageMonochrome,
+            'pageCoverageColor'           => $pageCoverageColor,
+            'adminCostPerPage'            => $adminCostPerPage,
+            'partsCostPerPage'            => $partsCostPerPage,
+            'laborCostPerPage'            => $laborCostPerPage,
+            'targetMonochromeCostPerPage' => $targetMonochromeCostPerPage,
+            'targetColorCostPerPage'      => $targetColorCostPerPage
+        );
+        $this->assertTrue($this->_form->isValid($data), "Hardware optimization setting form did not accept good data.");
+    }
+
+
+    /**
+     * Test the form using bad data
+     *
+     * @dataProvider badHOSettingData
+     */
+    public function testFormRejectsBadData ($name, $costThreshold, $pageCoverageMonochrome, $pageCoverageColor, $adminCostPerPage, $partsCostPerPage, $laborCostPerPage, $targetMonochromeCostPerPage, $targetColorCostPerPage)
+    {
+        $data = array(
+            'name'                        => $name,
+            'costThreshold'               => $costThreshold,
+            'pageCoverageMonochrome'      => $pageCoverageMonochrome,
+            'pageCoverageColor'           => $pageCoverageColor,
+            'adminCostPerPage'            => $adminCostPerPage,
+            'partsCostPerPage'            => $partsCostPerPage,
+            'laborCostPerPage'            => $laborCostPerPage,
+            'targetMonochromeCostPerPage' => $targetMonochromeCostPerPage,
+            'targetColorCostPerPage'      => $targetColorCostPerPage
+        );
+        $this->assertFalse($this->_form->isValid($data), "Hardware optimization setting form accepted bad data!");
+    }
+
+}
