b9537842ccbc335b891c25004b71ac24e3290383
lrobert <lrobert@tangentmtw.com>
2014-02-28 14:06:57 -0500

    Fixed device_instance_meter integrity constraints

--- migrations/20140228105458_fix_dealer_cascade_deletes.php
+++ migrations/20140228105458_fix_dealer_cascade_deletes.php
@@ -11,6 +11,7 @@ class FixDealerCascadeDeletes extends AbstractMigration
     public function up ()
     {
         $this->execute("ALTER TABLE device_instance_meters DROP FOREIGN KEY device_instance_meter_ibfk_1");
+        $this->execute("DELETE FROM device_instance_meters WHERE deviceInstanceId NOT IN (SELECT DISTINCT id FROM `device_instances`)");
         $this->execute("ALTER TABLE device_instance_meters ADD CONSTRAINT device_instance_meter_ibfk_1
                         FOREIGN KEY (`deviceInstanceId`)
                         REFERENCES `device_instances` (`id`)
