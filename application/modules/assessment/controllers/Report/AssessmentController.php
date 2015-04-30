b57b766525542a8bdec10cf857470853b9e34b81
Kate <kmccully@tangentmtw.com>
2015-04-30 11:32:26 -0400

    Updated DOCX rendering files to use new graphs. Removed references to old graphs.

--- application/modules/assessment/controllers/Report/AssessmentController.php
+++ application/modules/assessment/controllers/Report/AssessmentController.php
@@ -94,8 +94,7 @@ class Assessment_Report_AssessmentController extends Assessment_Library_Controll
         $this->initReportVariables($filename);
 
         // New graphs being passed to view
-        $assessmentViewModel->setNewGraphs($newGraphs);
-        $this->view->theGraphs = $this->cacheNewPNGImages($assessmentViewModel->getTheGraphs(), true, true);
+        $this->view->graphs = $this->cacheNewPNGImages($assessmentViewModel->getTheGraphs(), true, true);
 
         // Render early
         try
