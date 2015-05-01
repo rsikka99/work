928cd263142ad1d9199e5efbd6cbf953af01fff1
Erwin Kooi <ekooi@tangentmtw.com>
2015-05-01 09:38:19 -0400

    Enable Mac-style line-ending text files to be read properly

--- library/MPSToolbox/Legacy/Modules/ProposalGenerator/Services/RmsUpload/AbstractRmsUploadService.php
+++ library/MPSToolbox/Legacy/Modules/ProposalGenerator/Services/RmsUpload/AbstractRmsUploadService.php
@@ -336,6 +336,7 @@ abstract class AbstractRmsUploadService
                     $fileLines[] = $line;
                 }
             } else {
+                ini_set("auto_detect_line_endings", true);
                 $fileLines = file($filename, FILE_IGNORE_NEW_LINES);
             }
             $lineCount = count($fileLines) - 1 - $this->_linesToTrim;
