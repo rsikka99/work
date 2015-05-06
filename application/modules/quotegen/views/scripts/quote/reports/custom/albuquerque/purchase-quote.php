def63da9a90d4d7e0fc21b293eea2943660ca19d
Erwin Kooi <ekooi@tangentmtw.com>
2015-05-06 14:51:42 -0400

    Leased quote

--- application/modules/quotegen/views/scripts/quote/reports/custom/albuquerque/purchase-quote.php
+++ application/modules/quotegen/views/scripts/quote/reports/custom/albuquerque/purchase-quote.php
@@ -2,6 +2,6 @@
 
 $this->partial("{$commonPartsBasePath}/docx/titlepage.phtml", $titledata);
 $this->partial("{$basePath}/purchasequote.phtml", $data);
-$this->partial("{$basePath}/albuquerque/scope-of-work.phtml", $data);
-$this->partial("{$basePath}/albuquerque/obligations-and-exclusions.phtml", $data);
+$this->partial("{$customPath}/albuquerque/scope-of-work.phtml", $data);
+$this->partial("{$customPath}/albuquerque/obligations-and-exclusions.phtml", $data);
 $this->partial("{$basePath}/signatures.phtml", $data);
