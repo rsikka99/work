eb46fed8ff5134dee467d1cade1781a94bc4ffc2
Erwin Kooi <ek@cdyweb.com>
2015-06-18 14:52:33 -0400

    delete unneeded files

--- reloadDatabase.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo "Deleting Database..." . PHP_EOL;
-echo shell_exec("php scripts/createBlankDatabase.php");
-echo PHP_EOL;
-echo PHP_EOL;
-echo "Migrating..." . PHP_EOL;
-echo PHP_EOL;
-echo shell_exec("php ./vendor/robmorgan/phinx/bin/phinx migrate");
-echo PHP_EOL . "Finished!" . PHP_EOL;
