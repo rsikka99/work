0da1945252eb6f034d3d80e0d7c787d199cfd43d
unknown <ekooi@KMCCULLY-PC.tmtw.ca>
2016-08-24 10:13:15 -0400

    minor

--- library/Tangent/Ftp/NcFtp.php
+++ library/Tangent/Ftp/NcFtp.php
@@ -5,6 +5,7 @@ class NcFtp
 {
     public function get($url, $user, $pass, $dest) {
         $parts = parse_url($url);
+        $pass = str_replace('$','\$',$pass);
         $cmd = "ncftpget -C -u {$user} -p {$pass} {$parts['host']} {$parts['path']} {$dest}";
         $result = shell_exec($cmd);
         return $result;
