0269608fd42ba6bdc36f7e549aba4a94b84bb1cf
unknown <ekooi@KMCCULLY-PC.tmtw.ca>
2016-09-12 08:51:28 -0400

    LFM Api

---
+++ lfm.php
@@ -0,0 +1,70 @@
+<?php
+
+#--
+parse_str(implode('&', array_slice($argv, 1)), $_GET);
+if (empty($_GET['host'])) die('host?');
+if (empty($_GET['username'])) die('username?');
+if (empty($_GET['password'])) die('password?');
+unset($_GET['action']);
+#--
+
+ini_set("auto_detect_line_endings", true);
+
+defined('APPLICATION_BASE_PATH') || define('APPLICATION_BASE_PATH', realpath(dirname(__FILE__)));
+
+// Define the paths
+defined('APPLICATION_PATH') || define('APPLICATION_PATH', APPLICATION_BASE_PATH . '/application');
+defined('DATA_PATH') || define('DATA_PATH', APPLICATION_BASE_PATH . '/data');
+defined('PUBLIC_PATH') || define('PUBLIC_PATH', APPLICATION_BASE_PATH . '/public');
+defined('ASSETS_PATH') || define('ASSETS_PATH', APPLICATION_BASE_PATH . '/assets');
+
+/**
+ * This makes our life easier when dealing with paths. Everything is relative
+ * to the application root now.
+ */
+chdir(APPLICATION_BASE_PATH);
+
+// Setup autoloading
+require 'init_autoloader.php';
+My_Error_Handler::set();
+//
+//ini_set("display_errors", 1);
+//error_reporting(E_ALL);
+
+// Define application environment.
+defined('APPLICATION_ENV') || define('APPLICATION_ENV', (isset($_SERVER['php_environment_mode']) ? $_SERVER['php_environment_mode'] : 'production'));
+
+// Create application, bootstrap, and run
+$application = new Zend_Application('production', array(
+    'config' => array(
+        APPLICATION_PATH . '/configs/global.php',
+        APPLICATION_PATH . '/configs/local.php',
+    )));
+$application->bootstrap();
+
+//##########################################################################################################
+
+$auth   = Zend_Auth::getInstance();
+$user = json_decode(json_encode(['id'=>1, 'eulaAccepted'=>true, 'firstname'=>'unit', 'lastname'=>'testing', 'dealerId'=>1, 'resetPasswordOnNextLogin'=>false, 'email'=>'it@tangentmtw.com']));
+$auth->getStorage()->write($user);
+$mpsSession = new Zend_Session_Namespace('mps-tools');
+$mpsSession->selectedClientId = 1;
+$mpsSession->selectedRmsUploadId=1;
+
+try {
+
+    $lfm = new \MPSToolbox\Api\LFM($_GET);
+    //var_dump($lfm->licensed());
+    //var_dump($lfm->version());
+    $clients = $lfm->client();
+    foreach ($clients['ClientDetails'] as $client) {
+        var_dump($client);
+        $printers = $lfm->getPrinterSupplyHistoryForClient($client['ClientName'], date('Y-m-d 00:00'), date('Y-m-d 23:59'));
+        var_dump($printers);
+    }
+
+    #var_dump($lfm->getPrintersForClient());
+
+} catch (Exception $ex) {
+    var_dump($ex);
+}
---
+++ library/MPSToolbox/Api/LFM.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace MPSToolbox\Api;
+
+use PhpOffice\PhpWord\Exception\Exception;
+
+class LFM {
+
+    public $host = null;
+    public $format = 'json';
+    public $debug = false;
+
+    public $username = '';
+    public $password = '';
+
+    /**
+     * @var \GuzzleHttp\Client
+     */
+    private $session = null;
+
+    public function __construct($attributes) {
+        foreach(['host','username','password','debug'] as $k)
+        if (isset($attributes[$k])) {
+            $this->$k = $attributes[$k];
+        }
+    }
+
+    /**
+     * @return \GuzzleHttp\Client
+     */
+    private function getSession() {
+        if (empty($this->session)) {
+            $opt=[
+                'allow_redirects' => false,
+                'verify'=>false,
+                'base_uri' => 'http://'.$this->host.'/com.lexmark.lfm.api/'.$this->format.'/',
+                'cookies' => true,
+                'debug' => $this->debug,
+            ];
+            $this->session = new \GuzzleHttp\Client($opt);
+        }
+        return $this->session;
+    }
+
+    private function request($method, $query=null) {
+        $session = $this->getSession();
+
+        $response = $session->get($method, $query ? ['query' => $query] : []);
+
+        if (($response->getStatusCode()==302) || ($response->getStatusCode() == 301)) {
+            if (preg_match('#/login/auth#',$response->getHeaderLine('Location'))) {
+                /**/
+                $auth_response = $this->session->get($response->getHeaderLine('Location'));
+                if ($auth_response->getStatusCode() != 200) {
+                    throw new Exception('Cannot connect to LFM auth page, status code='.$response->getStatusCode());
+                }
+
+                $auth_response = $this->session->post('/j_spring_security_check', [
+                    'form_params' => [
+                        'j_username'=>$this->username,
+                        'j_password'=>$this->password,
+                    ]
+                ]);
+                if (($auth_response->getStatusCode()==302) || ($auth_response->getStatusCode()==301)) {
+                    if (preg_match('#'.$method.'#',$auth_response->getHeaderLine('Location'))) {
+
+                        $response = $session->get($method, $query ? ['query' => $query] : []);
+
+                    }
+                } else {
+                    throw new Exception('Login failed, status code='.$response->getStatusCode());
+                }
+            }
+        }
+        if ($response->getStatusCode() != 200) {
+            throw new Exception('Request failed, status code='.$response->getStatusCode());
+        }
+
+        $body = $response->getBody()->getContents();
+
+        switch($this->format) {
+            case 'json': {
+                $result = json_decode($body, true);
+                return $result;
+            }
+        }
+
+        return null;
+    }
+
+    public function licensed() {
+        return $this->request('licensed');
+    }
+
+    public function version() {
+        return $this->request('version');
+    }
+
+    public function client() {
+        return $this->request('client');
+    }
+
+    public function getPrintersForClient($clientid, $start=null, $limit=null) {
+        $query = ['clientid'=>$clientid];
+        if ($start) $query['start'] = $start;
+        if ($limit) $query['limit'] = $limit;
+        return $this->request('getprintersforclient', $query);
+    }
+
+    public function getPrinterCountHistoryForClient($clientid, $starttime=null, $endtime=null, $start=null, $limit=null) {
+        $query = ['clientid'=>$clientid];
+        if ($start) $query['start'] = $start;
+        if ($limit) $query['limit'] = $limit;
+        if ($limit) $query['starttime'] = $starttime;
+        if ($limit) $query['endtime'] = $endtime;
+        return $this->request('getprintercounthistoryforclient', $query);
+    }
+
+    public function getPrinterStatusHistoryForClient($clientid, $starttime=null, $endtime=null, $start=null, $limit=null) {
+        $query = ['clientid'=>$clientid];
+        if ($start) $query['start'] = $start;
+        if ($limit) $query['limit'] = $limit;
+        if ($limit) $query['starttime'] = $starttime;
+        if ($limit) $query['endtime'] = $endtime;
+        return $this->request('getprinterstatushistoryforclient', $query);
+    }
+
+    public function getPrinterSupplyHistoryForClient($clientid, $starttime=null, $endtime=null, $start=null, $limit=null) {
+        $query = ['clientid'=>$clientid];
+        if ($start) $query['start'] = $start;
+        if ($limit) $query['limit'] = $limit;
+        if ($limit) $query['starttime'] = $starttime;
+        if ($limit) $query['endtime'] = $endtime;
+        return $this->request('getprintersupplyhistoryforclient', $query);
+    }
+
+
+}
