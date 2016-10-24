df3c302b3ac363ccfaf47ca9c6494c367bbad009
Erwin Kooi <github@cdyweb.com>
2016-10-24 13:58:44 -0400

    Set margins by manufacturer and type

--- application/modules/ecommerce/controllers/CatalogController.php
+++ application/modules/ecommerce/controllers/CatalogController.php
@@ -164,19 +164,28 @@ class Ecommerce_CatalogController extends Action {
             $st = $db->prepare('replace into dealer_category set categoryId=?, dealerId=?, name=?, taxable=?, orderBy=?');
             $st->execute([$categoryId, $dealerId, $this->getParam('name'), $this->getParam('taxable'), $this->getParam('orderBy')]);
 
-            $service->replaceCategoryPriceLevel($dealerId, $categoryId, $this->getParam('margin'));
+            $manufacturerId = $this->getParam('manufacturer');
+            $type = $this->getParam('type');
+            $useDefaultMargins = $this->getParam('useDefaultMargins');
+
+            if (!$manufacturerId) $manufacturerId=null;
+            if (!$type) $type=null;
+
+            if ($manufacturerId || $type) {
+                if ($useDefaultMargins) {
+                    $service->deleteCategoryPriceLevel($dealerId, $categoryId, $manufacturerId, $type);
+                    echo json_encode(['ok'=>true]);
+                    return;
+                }
+            }
+
+            $service->replaceCategoryPriceLevel($dealerId, $categoryId, $manufacturerId, $type, $this->getParam('margin'));
             echo json_encode(['ok'=>true]);
             return;
         }
 
-        $dealer_price_levels = $service->listByDealerAndCategory($dealerId, $categoryId);
-        $margins = '';
-        foreach ($dealer_price_levels as $price_line) {
-            $margins .= '<tr>';
-            $margins .= '<td>'.$price_line['name'].'</td>';
-            $margins .= '<td><input required="required" class="form-control" type="number" step="0.1" value="'.($price_line['category_margin']?$price_line['category_margin']:$price_line['margin']).'" name="margin['.$price_line['id'].']"></td>';
-            $margins .= '</tr>';
-        }
+        $dealer_price_levels = $service->listByDealerAndCategory($dealerId, $categoryId, null, null);
+        $margins = $this->marginTable($dealer_price_levels);
 
         $editFunction = 'editSku';
         $showAdd = true;
@@ -184,6 +193,25 @@ class Ecommerce_CatalogController extends Action {
 
         $category = $db->query('select * from base_category where id='.$categoryId)->fetch(PDO::FETCH_ASSOC);
         $dealer_category = $db->query('select * from dealer_category where dealerId='.$dealerId.' and categoryId='.$categoryId)->fetch(PDO::FETCH_ASSOC);
+
+        $mfgSelect = '<option value=""> - default - </option>';
+        $typeSelect = '<option value=""> - default - </option>';
+
+        foreach ($db->query('select manufacturerId, displayname from base_product join manufacturers on base_product.manufacturerId=manufacturers.id where categoryId='.$categoryId.' group by manufacturerId order by displayname') as $line) {
+            $mfgSelect.='<option value="'.$line['manufacturerId'].'">'.$line['displayname'].'</option>';
+        }
+
+        switch ($categoryId) {
+            case 90881 :  // oem supplies
+            case 90882 : {
+                foreach ($db->query('select distinct(type) as t from base_printer_consumable order by t') as $line) {
+                    if ($line['t']) {
+                        $typeSelect.='<option value="'.$line['t'].'">'.$line['t'].'</option>';
+                    }
+                }
+                break;
+            }
+        }
 
         $result=[
             'name'=>    isset($dealer_category['name'])     ? $dealer_category['name'] : $category['name'],
@@ -193,6 +221,8 @@ class Ecommerce_CatalogController extends Action {
             'showAdd' => $showAdd?true:false,
             'margins'=>$margins,
             'products'=>$products,
+            'mfgSelect'=>$mfgSelect,
+            'typeSelect'=>$typeSelect,
         ];
 
         echo json_encode($result);
@@ -230,6 +260,51 @@ class Ecommerce_CatalogController extends Action {
         $st = $db->prepare("select concat(supplierId,';',supplierSku) as id, vpn, description, upc, price from supplier_product join supplier_price using (supplierId, supplierSku) where baseProductId is null and dealerId=? and (vpn like ? or description like ?) group by vpn");
         $st->execute([$dealerId, $q, $q]);
         echo json_encode($st->fetchAll());
+    }
+
+    private function marginTable($dealer_price_levels, $ext_price_levels=null) {
+        $margins = '';
+        foreach ($dealer_price_levels as $i=>$price_line) {
+            $defaultValue = $actualValue = $price_line['category_margin']?$price_line['category_margin']:$price_line['margin'];
+            foreach ($ext_price_levels as $e) {
+                if ($e['id']==$price_line['id']) {
+                    if ($e['category_margin']) {
+                        $actualValue = $e['category_margin'];
+                    }
+                }
+            }
+            $margins .= '<tr>';
+            $margins .= '<td>'.$price_line['name'].'</td>';
+            $margins .= '<td><input data-default="'.$defaultValue.'" data-actual="'.$actualValue.'" required="required" class="form-control" type="number" step="0.1" value="'.$actualValue.'" name="margin['.$price_line['id'].']"></td>';
+            $margins .= '</tr>';
+        }
+        return $margins;
+    }
+
+    public function mfgOrTypeAction() {
+        $mfg = $this->getParam('mfg');
+        $type = $this->getParam('type');
+
+        if (!$mfg) $mfg = null;
+        if (!$type) $type = null;
+
+        $categoryId = intval($this->getParam('id'));
+        $dealerId = intval(\MPSToolbox\Entities\DealerEntity::getDealerId());
+
+        $service = new \MPSToolbox\Services\PriceLevelService();
+
+        $dealer_price_levels = $service->listByDealerAndCategory($dealerId, $categoryId, null, null);
+        $ext_price_levels = $service->listByDealerAndCategory($dealerId, $categoryId, $mfg, $type);
+
+        $margins = $this->marginTable($dealer_price_levels, $ext_price_levels);
+        $useDefaultMargins = true;
+        foreach ($ext_price_levels as $line) if ($line['category_margin']) $useDefaultMargins=false;
+
+        $result = [
+            'useDefaultMargins'=>$useDefaultMargins,
+            'margins'=>$margins,
+        ];
+        $this->sendJson($result);
     }
 
 }
