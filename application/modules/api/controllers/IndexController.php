3582d0ff9ded152f31ee98485ad8a43c43780192
Erwin Kooi <github@cdyweb.com>
2016-10-25 13:22:01 -0400

    Support popup

--- application/modules/api/controllers/IndexController.php
+++ application/modules/api/controllers/IndexController.php
@@ -6,5 +6,27 @@ use Tangent\Controller\Action;
  */
 class Api_IndexController extends Action
 {
+    public function supportAction() {
+        $config = \Zend_Registry::get('config');
 
+        $mail = new Zend_Mail ();
+        $mail->setFrom($config->app->supportEmail, 'MPSToolbox Support');
+        $mail->addTo($config->app->supportEmail, 'MPSToolbox Support');
+        if (@$_POST['Email']) $mail->setReplyTo($_POST['Email']);
+        $mail->setSubject('MPSToolbox Support');
+
+        $tr = '';
+        foreach ($_POST as $k=>$v) {
+            $tr .= '<tr><td>'.$k.': </td><td>'.$v.'</td></tr>';
+        }
+
+        $body = "<h2>MPSToolbox Support</h2>";
+        $body .= "<table>";
+        $body .= $tr;
+        $body .= "</table>";
+        $mail->setBodyHtml($body);
+        $mail->send();
+
+        $this->sendJson(['ok'=>true]);
+    }
 }
