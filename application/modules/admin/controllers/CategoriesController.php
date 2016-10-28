3f19bf7e242ceae927bef93b7b22abf5c8d1d38e
Erwin Kooi <github@cdyweb.com>
2016-10-28 13:55:02 -0400

    Cat editor

--- application/modules/admin/controllers/CategoriesController.php
+++ application/modules/admin/controllers/CategoriesController.php
@@ -9,11 +9,18 @@ class Admin_CategoriesController extends Action
 
     public function indexAction ()
     {
+        $this->view->error=false;
+
         $db = Zend_Db_Table::getDefaultAdapter();
         if ($this->getParam('delete')) {
-            $db->query('delete from base_category where id=' . intval($this->getParam('delete')));
-            header('Location: /admin/categories');
-            exit();
+            $n = $db->query('select count(*) from base_product where categoryId='.intval($this->getParam('delete')))->fetchColumn(0);
+            if ($n>0) {
+                $this->view->error="Cannot delete this category because it contains products.";
+            } else {
+                $db->query('DELETE FROM base_category WHERE id=' . intval($this->getParam('delete')));
+                header('Location: /admin/categories');
+                exit();
+            }
         }
 
         $this->_pageTitle = ['Catalog Categories'];
@@ -40,14 +47,16 @@ class Admin_CategoriesController extends Action
         $db = Zend_Db_Table::getDefaultAdapter();
 
         if ($this->getRequest()->getMethod()=='POST') {
-            $st = $db->prepare('update base_category set name=?, parent=? where id=?');
-            $st->execute([$this->getParam('name'), $this->getParam('parent')?$this->getParam('parent'):null, $this->getParam('id')]);
+            $st = $db->prepare('update base_category set name=?, parent=?, properties=? where id=?');
+            $st->execute([$this->getParam('name'), $this->getParam('parent')?$this->getParam('parent'):null, $this->getParam('properties'), $this->getParam('id')]);
             header('Location: /admin/categories');
             exit();
         }
 
         $this->view->base_category = $db->query('select * from base_category order by name')->fetchAll();
         $this->view->edit = $db->query('select * from base_category where id='.intval($this->getParam('id')))->fetch();
+
+        $this->_pageTitle = ['Edit Category'];
     }
 }
 
